92a34a40e0a56382d47252a09f1a238bcfdc5469
Nayuki Minase <nayuki@eigenstate.org>
2008-04-17 01:56:11 +0000

    Reformatted code; made minor edits
    git-svn-id: file:///home/user/p79068-java-library-svn@6 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/util/JavaRandomAdapter.java
+++ Source/p79068/util/JavaRandomAdapter.java
@@ -1,23 +1,30 @@
 package p79068.util;
 
 
-final class JavaRandomAdapter extends java.util.Random{
-
- Random random;
-
-
- JavaRandomAdapter(Random r){
-  random=r;}
-
-
- protected int next(int bits){
-  return random.randomInt()>>>(32-bits);}
-
- public int nextInt(){
-  return random.randomInt();}
-
- public long nextLong(){
-  return random.randomLong();}
-
- public void setSeed(long seed){
-  throw new UnsupportedOperationException();}}
+final class JavaRandomAdapter extends java.util.Random {
+	
+	private Random random;
+	
+	
+	public JavaRandomAdapter(Random rand) {
+		random = rand;
+	}
+	
+	
+	protected int next(int bits) {
+		return random.randomInt() >>> (32 - bits);
+	}
+	
+	public int nextInt() {
+		return random.randomInt();
+	}
+	
+	public long nextLong() {
+		return random.randomLong();
+	}
+	
+	public void setSeed(long seed) {
+		throw new UnsupportedOperationException();
+	}
+	
+}
