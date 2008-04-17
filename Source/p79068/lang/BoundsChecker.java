62838cbd6b55013ea6291285957087d0bc50cd7a
Nayuki Minase <nayuki@eigenstate.org>
2008-04-17 02:20:14 +0000

    Automatically reformatted code
    git-svn-id: file:///home/user/p79068-java-library-svn@8 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/lang/BoundsChecker.java
+++ Source/p79068/lang/BoundsChecker.java
@@ -5,27 +5,32 @@ package p79068.lang;
 Provides convenience methods for checking bounds and throwing IndexOutOfBoundsExceptions.
 <p>Instantiability: <em>Not applicable</em></p>
 */
-public final class BoundsChecker{
-
- /**
- Throws an IndexOutOfBoundsException if the specified index is outside the specified bounds; otherwise does nothing.
- <p>An index is out of bounds if:</p>
- <ul><li><code>accessindex &lt; 0</code></li>
-  <li><code>accessindex &ge; arraylength</code></li></ul>
- */
- public static void check(int arrayLength,int accessIndex){
-  if(accessIndex<0||accessIndex>=arrayLength)throw new IndexOutOfBoundsException(String.format("Bounds = [%d,%d), access index = %d",0,arrayLength,accessIndex));}
-
- /**
- Throws an IndexOutOfBoundsException if the specified range is outside the specified bounds; otherwise does nothing.
- <p>An index is out of bounds if:</p>
- <ul><li><code>accessoff &lt; 0</code></li>
-  <li><code>accessoff &gt; arraylength</code></li>
-  <li><code>accesslen &lt; 0</code></li>
-  <li><code>accessoff+accesslen &gt; arraylength</code></li></ul>
- */
- public static void check(int arrayLength,int accessOff,int accessLen){
-  if(accessOff<0||accessOff>arrayLength||accessLen<0||accessOff+accessLen>arrayLength)throw new IndexOutOfBoundsException(String.format("Bounds = [%d,%d), access range = [%d,%d)",0,arrayLength,accessOff,accessOff+accessLen));}
-
-
- private BoundsChecker(){}}
+public final class BoundsChecker {
+	
+	/**
+	Throws an IndexOutOfBoundsException if the specified index is outside the specified bounds; otherwise does nothing.
+	<p>An index is out of bounds if:</p>
+	<ul><li><code>accessindex &lt; 0</code></li>
+	 <li><code>accessindex &ge; arraylength</code></li></ul>
+	*/
+	public static void check(int arrayLength, int accessIndex) {
+		if (accessIndex < 0 || accessIndex >= arrayLength)
+			throw new IndexOutOfBoundsException(String.format("Bounds = [%d,%d), access index = %d", 0, arrayLength, accessIndex));
+	}
+	
+	/**
+	Throws an IndexOutOfBoundsException if the specified range is outside the specified bounds; otherwise does nothing.
+	<p>An index is out of bounds if:</p>
+	<ul><li><code>accessoff &lt; 0</code></li>
+	 <li><code>accessoff &gt; arraylength</code></li>
+	 <li><code>accesslen &lt; 0</code></li>
+	 <li><code>accessoff+accesslen &gt; arraylength</code></li></ul>
+	*/
+	public static void check(int arrayLength, int accessOff, int accessLen) {
+		if (accessOff < 0 || accessOff > arrayLength || accessLen < 0 || accessOff + accessLen > arrayLength)
+			throw new IndexOutOfBoundsException(String.format("Bounds = [%d,%d), access range = [%d,%d)", 0, arrayLength, accessOff, accessOff + accessLen));
+	}
+	
+	
+	private BoundsChecker() {}
+}
--- Source/p79068/lang/NullChecker.java
+++ Source/p79068/lang/NullChecker.java
@@ -5,11 +5,14 @@ package p79068.lang;
 Provides a convenience method for checking for <code>null</code> and throwing NullPointerExceptions.
 <p>Instantiability: <em>Not applicable</em></p>
 */
-public final class NullChecker{
-
- /** Throws a NullPointerException if the specified object is null; otherwise does nothing. */
- public static void check(Object obj){
-  if(obj==null)throw new NullPointerException();}
-
-
- private NullChecker(){}}
+public final class NullChecker {
+	
+	/** Throws a NullPointerException if the specified object is null; otherwise does nothing. */
+	public static void check(Object obj) {
+		if (obj == null)
+			throw new NullPointerException();
+	}
+	
+	
+	private NullChecker() {}
+}
