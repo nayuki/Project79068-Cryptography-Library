c6cd4cf72970b44e4ba321b61df5e36eeebfb069
Nayuki Minase <nayuki@eigenstate.org>
2008-04-18 15:06:18 +0000

    Updated documentation comments
    git-svn-id: file:///home/user/p79068-java-library-svn@61 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/datastruct/Queue.java
+++ Source/p79068/datastruct/Queue.java
@@ -8,34 +8,42 @@ public interface Queue<E> extends Cloneable {
 	
 	/**
 	 * Adds the specified object to the tail of this queue.
+	 * @param obj the object to enqueue
 	 */
 	public void enqueue(E obj);
 	
 	
 	/**
-	 * Removes and returns the head of this queue.
+	 * Removes and returns the object at the head of this queue.
+	 * @return the object at the head of this queue
 	 */
 	public E dequeue();
 	
 	
 	/**
-	 * Returns the head of this queue.
+	 * Returns the object at head of this queue without removing it.
+	 * @return the object at the head of this queue
 	 */
 	public E peek();
 	
 	
 	/**
 	 * Tests whether this queue is empty.
+	 * @return <code>true</true> if this queue has no objects; <code>false</code> otherwise
 	 */
 	public boolean isEmpty();
 	
 	
+	/**
+	 * Creates and returns a copy of this queue.
+	 * @return a copy of this queue
+	 */
 	public Queue<E> clone();
 	
 	
 	/**
-	 * Returns a string representation of the queue. Currently, the contents of the queue are listed from head to tail. This is subjected to change.
-	 * @return a string representation of the queue
+	 * Returns a string representation of this queue. A suggested format is <code>Queue [<var>head</var>, ..., <var>tail</var>]</code>. This is subjected to change.
+	 * @return a string representation of this queue
 	 */
 	public String toString();
 	
--- Source/p79068/datastruct/Stack.java
+++ Source/p79068/datastruct/Stack.java
@@ -8,34 +8,42 @@ public interface Stack<E> extends Cloneable {
 	
 	/**
 	 * Adds the specified object to the top of this stack.
+	 * @param obj the object to add
 	 */
 	public void push(E obj);
 	
 	
 	/**
 	 * Removes and returns the object at the top of this stack.
+	 * @return the object at the top of this stack
 	 */
 	public E pop();
 	
 	
 	/**
-	 * Returns the object at the top of this stack.
+	 * Returns the object at the top of this stack without removing it.
+	 * @return the object at the top of this stack
 	 */
 	public E peek();
 	
 	
 	/**
 	 * Tests whether this stack is empty.
+	 * @return <code>true</true> if this stack has no objects; <code>false</code> otherwise
 	 */
 	public boolean isEmpty();
 	
 	
+	/**
+	 * Creates and returns a copy of this stack.
+	 * @return a copy of this stack
+	 */
 	public Stack<E> clone();
 	
 	
 	/**
-	 * Returns a string representation of the stack. Currently, the contents of the stack are listed from bottom to top. This is subjected to change.
-	 * @return a string representation of the stack
+	 * Returns a string representation of this stack. A suggested format is <code>Queue [<var>bottom</var>, ..., <var>top</var>]</code>. This is subjected to change.
+	 * @return a string representation of this stack
 	 */
 	public String toString();
 	
