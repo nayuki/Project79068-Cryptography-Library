66ef0c07dcb5a09a5b175f74c166794f229cfc1e
Nayuki Minase <nayuki@eigenstate.org>
2008-04-19 14:32:12 +0000

    Added initial version of newly rewritten data structure classes
    git-svn-id: file:///home/user/p79068-java-library-svn@108 24ebd69f-44c7-534f-88b9-abd8d07edc6a

---
+++ Source/p79068/datastruct/DictionaryEntry.java
@@ -0,0 +1,15 @@
+package p79068.datastruct;
+
+
+public class DictionaryEntry<K,V> {
+	
+	public final K key;
+	public final V value;
+	
+	
+	public DictionaryEntry(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+	
+}
