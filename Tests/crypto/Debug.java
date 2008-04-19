52d09d62522d0d5adb4858eff002d970dbfacb79
Nayuki Minase <nayuki@eigenstate.org>
2008-04-19 02:58:40 +0000

    Automatically reformatted code
    git-svn-id: file:///home/user/p79068-java-library-svn@93 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Tests/crypto/Debug.java
+++ Tests/crypto/Debug.java
@@ -1,36 +1,49 @@
 package crypto;
 
 
-class Debug{
-
- static byte[] hexToBytes(String s){
-  if(s.length()%2!=0)throw new IllegalArgumentException();
-  byte[] b=new byte[s.length()/2];
-  for(int i=0;i<b.length;i++)b[i]=(byte)Integer.parseInt(s.substring(i*2,(i+1)*2),16);
-  return b;}
-
- static String bytesToHex(byte[] b){
-  StringBuffer sb=new StringBuffer();
-  for(int i=0;i<b.length;i++){
-   sb.append(Integer.toString(b[i]>>>4&0xF,16));
-   sb.append(Integer.toString(b[i]>>>0&0xF,16));}
-  return sb.toString();}
-
-
- static byte[] asciiToBytes(String s){
-  byte[] b=new byte[s.length()];
-  for(int i=0;i<b.length;i++){
-   char c=s.charAt(i);
-   if(c>=0x80)c='?';
-   b[i]=(byte)c;}
-  return b;}
-
- static String bytesToAscii(byte[] b){
-  StringBuffer sb=new StringBuffer();
-  for(int i=0;i<b.length;i++){
-   if((b[i]&0xFF)<0x80)sb.append((char)(b[i]&0xFF));
-   else sb.append('?');}
-  return sb.toString();}
-
-
- private Debug(){}}
+class Debug {
+	
+	static byte[] hexToBytes(String s) {
+		if (s.length() % 2 != 0)
+			throw new IllegalArgumentException();
+		byte[] b = new byte[s.length() / 2];
+		for (int i = 0; i < b.length; i++)
+			b[i] = (byte)Integer.parseInt(s.substring(i * 2, (i + 1) * 2), 16);
+		return b;
+	}
+	
+	static String bytesToHex(byte[] b) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < b.length; i++) {
+			sb.append(Integer.toString(b[i] >>> 4 & 0xF, 16));
+			sb.append(Integer.toString(b[i] >>> 0 & 0xF, 16));
+		}
+		return sb.toString();
+	}
+	
+	
+	static byte[] asciiToBytes(String s) {
+		byte[] b = new byte[s.length()];
+		for (int i = 0; i < b.length; i++) {
+			char c = s.charAt(i);
+			if (c >= 0x80)
+				c = '?';
+			b[i] = (byte)c;
+		}
+		return b;
+	}
+	
+	static String bytesToAscii(byte[] b) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < b.length; i++) {
+			if ((b[i] & 0xFF) < 0x80)
+				sb.append((char)(b[i] & 0xFF));
+			else
+				sb.append('?');
+		}
+		return sb.toString();
+	}
+	
+	
+	private Debug() {}
+}
