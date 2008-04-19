e7363c08966da5cff0aeb0c853287210041e29cf
Nayuki Minase <nayuki@eigenstate.org>
2008-04-19 02:48:32 +0000

    Removed unnecessary methods
    git-svn-id: file:///home/user/p79068-java-library-svn@92 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Tests/crypto/Debug.java
+++ Tests/crypto/Debug.java
@@ -33,11 +33,4 @@ class Debug{
   return sb.toString();}
 
 
- static boolean areEqual(byte[] a,byte[] b){
-  if(a.length!=b.length)return false;
-  for(int i=0;i<a.length;i++){
-   if(a[i]!=b[i])return false;}
-  return true;}
-
-
  private Debug(){}}
