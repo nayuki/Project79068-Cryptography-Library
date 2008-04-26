3cdd5d91f5ddaebd6a225f03786fcc7f5901fa00
Nayuki Minase <nayuki@eigenstate.org>
2008-04-26 01:19:07 +0000

    Manually reformatted code
    git-svn-id: file:///home/user/p79068-java-library-svn@153 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/crypto/cipher/Rijndael.java
+++ Source/p79068/crypto/cipher/Rijndael.java
@@ -2,24 +2,31 @@ package p79068.crypto.cipher;
 
 
 /**
-The Rijndael/AES (Advanced Encryption Standard) block cipher.
-<p>Key lengths: Any positive multiple of 32 bits (4 bytes)</p>
-<p>Block lengths: 128, 160, 192, 224, and 256 bits. While Rijndael can use all of these, AES is limited to 128 bits (16 bytes).</p>
-*/
+ * The Rijndael/AES (Advanced Encryption Standard) block cipher.
+ * <p>Key lengths: Any positive multiple of 32 bits (4 bytes)</p>
+ * <p>Block lengths: 128, 160, 192, 224, and 256 bits. While Rijndael can use all of these, AES is limited to 128 bits (16 bytes).</p>
+ */
 public final class Rijndael extends BlockCipher {
 	
-	/** The AES cipher algorithm with 128-bit key size. */
+	/**
+	 * The AES cipher algorithm with 128-bit key size.
+	 */
 	public static final Rijndael AES128_CIPHER = new Rijndael(16, 16);
 	
-	/** The AES cipher algorithm with 192-bit key size. */
+	/**
+	 * The AES cipher algorithm with 192-bit key size.
+	 */
 	public static final Rijndael AES192_CIPHER = new Rijndael(24, 16);
 	
-	/** The AES cipher algorithm with 256-bit key size. */
+	/** The AES cipher algorithm with 256-bit key size.
+	 */
 	public static final Rijndael AES256_CIPHER = new Rijndael(32, 16);
 	
-
+	
+	
 	private int keyLength;
 	private int blockLength;
+	
 	
 	
 	public Rijndael(int keyLength, int blockLength) {
@@ -32,6 +39,7 @@ public final class Rijndael extends BlockCipher {
 	}
 	
 	
+	
 	public Cipherer newCipherer(byte[] key) {
 		if (key.length != keyLength)
 			throw new IllegalArgumentException();
@@ -42,17 +50,23 @@ public final class Rijndael extends BlockCipher {
 	}
 	
 	
-	/** Returns the name of this cipher algorithm: <samp>Rijndael (<var>n</var>-bit key, <var>m</var>-bit block)</samp>. */
+	/**
+	 * Returns the name of this cipher algorithm: <samp>Rijndael (<var>n</var>-bit key, <var>m</var>-bit block)</samp>.
+	 */
 	public String getName() {
 		return String.format("Rijndael (%d-bit key, %d-bit block)", keyLength * 8, blockLength * 8);
 	}
 	
-	/** Returns the key length of this cipher algorithm. */
+	/**
+	 * Returns the key length of this cipher algorithm.
+	 */
 	public int getKeyLength() {
 		return keyLength;
 	}
 	
-	/** Returns the block length of this cipher algorithm. */
+	/**
+	 * Returns the block length of this cipher algorithm.
+	 */
 	public int getBlockLength() {
 		return blockLength;
 	}
@@ -65,7 +79,9 @@ public final class Rijndael extends BlockCipher {
 		return keyLength == cipher.keyLength && blockLength == cipher.blockLength;
 	}
 	
+	
 	public int hashCode() {
 		return keyLength << 16 | blockLength;
 	}
+	
 }
