336c3b320ad215b0d8d90409b17dc2e4bb7b7d1c
Nayuki Minase <nayuki@eigenstate.org>
2008-04-26 01:25:48 +0000

    Updated tests to use the refactored helper method
    git-svn-id: file:///home/user/p79068-java-library-svn@155 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Tests/crypto/cipher/TeaTest.java
+++ Tests/crypto/cipher/TeaTest.java
@@ -1,10 +1,7 @@
 package crypto.cipher;
 
-import static org.junit.Assert.assertArrayEquals;
 import org.junit.Test;
 import crypto.CryptoUtils;
-import p79068.crypto.cipher.Cipher;
-import p79068.crypto.cipher.Cipherer;
 import p79068.crypto.cipher.Tea;
 import p79068.crypto.cipher.Xtea;
 
@@ -13,35 +10,19 @@ public class TeaTest {
 	
 	@Test
 	public void testTea() {
-		test(Tea.CIPHER, "00000000800000000000000000000000", "0000000000000000", "9327c49731b08bbe");
-		test(Tea.CIPHER, "80000000000000000000000000000000", "0000000000000000", "9327c49731b08bbe");
-		test(Tea.CIPHER, "80000000000000008000000080000000", "0000000000000000", "9327c49731b08bbe");
-		test(Tea.CIPHER, "00000000800000008000000080000000", "0000000000000000", "9327c49731b08bbe");
+		CryptoUtils.testCipher(Tea.CIPHER, "00000000800000000000000000000000", "0000000000000000", "9327c49731b08bbe");
+		CryptoUtils.testCipher(Tea.CIPHER, "80000000000000000000000000000000", "0000000000000000", "9327c49731b08bbe");
+		CryptoUtils.testCipher(Tea.CIPHER, "80000000000000008000000080000000", "0000000000000000", "9327c49731b08bbe");
+		CryptoUtils.testCipher(Tea.CIPHER, "00000000800000008000000080000000", "0000000000000000", "9327c49731b08bbe");
 	}
 	
 	
 	@Test
 	public void testXtea() {
-		test(Xtea.CIPHER, "00000000800000000000000000000000", "0000000000000000", "4f190ccfc8deabfc");
-		test(Xtea.CIPHER, "80000000000000000000000000000000", "0000000000000000", "057e8c0550151937");
-		test(Xtea.CIPHER, "80000000000000008000000080000000", "0000000000000000", "31c4e2c6b347b2de");
-		test(Xtea.CIPHER, "00000000800000008000000080000000", "0000000000000000", "ed69b78566781ef3");
-	}
-	
-	
-	
-	private static void test(Cipher cipher, String key, String plaintext, String ciphertext) {
-		test(cipher, CryptoUtils.hexToBytes(key), CryptoUtils.hexToBytes(plaintext), CryptoUtils.hexToBytes(ciphertext));
-	}
-	
-	
-	private static void test(Cipher cipher, byte[] key, byte[] plaintext, byte[] ciphertext) {
-		byte[] originalplaintext = plaintext.clone();
-		Cipherer cipherer = cipher.newCipherer(key);
-		cipherer.encrypt(plaintext);
-		assertArrayEquals(ciphertext, plaintext);
-		cipherer.decrypt(plaintext);
-		assertArrayEquals(originalplaintext, plaintext);
+		CryptoUtils.testCipher(Xtea.CIPHER, "00000000800000000000000000000000", "0000000000000000", "4f190ccfc8deabfc");
+		CryptoUtils.testCipher(Xtea.CIPHER, "80000000000000000000000000000000", "0000000000000000", "057e8c0550151937");
+		CryptoUtils.testCipher(Xtea.CIPHER, "80000000000000008000000080000000", "0000000000000000", "31c4e2c6b347b2de");
+		CryptoUtils.testCipher(Xtea.CIPHER, "00000000800000008000000080000000", "0000000000000000", "ed69b78566781ef3");
 	}
 	
 }
