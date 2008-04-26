e9ed2d4f2a4aaf625392d4a6066ba640e68299d0
Nayuki Minase <nayuki@eigenstate.org>
2008-04-26 03:11:33 +0000

    Added test cases
    git-svn-id: file:///home/user/p79068-java-library-svn@174 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Tests/util/hash/Xor8Test.java
+++ Tests/util/hash/Xor8Test.java
@@ -10,6 +10,9 @@ public class Xor8Test {
 	@Test
 	public void basic() {
 		CryptoUtils.testWithAsciiMessage(Xor8.FUNCTION, "", "00");
+		CryptoUtils.testWithAsciiMessage(Xor8.FUNCTION, "bb", "00");
+		CryptoUtils.testWithAsciiMessage(Xor8.FUNCTION, "asdf", "10");
+		CryptoUtils.testWithAsciiMessage(Xor8.FUNCTION, "fads", "10");
 		CryptoUtils.testWithAsciiMessage(Xor8.FUNCTION, "The", "59");
 		CryptoUtils.testWithAsciiMessage(Xor8.FUNCTION, "tEh", "59");
 		CryptoUtils.testWithAsciiMessage(Xor8.FUNCTION, "soy", "65");
