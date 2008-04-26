1587be9339fd0380ba766b0c1ec8e60d2220a5bb
Nayuki Minase <nayuki@eigenstate.org>
2008-04-26 02:50:09 +0000

    Updated test case to use new method
    git-svn-id: file:///home/user/p79068-java-library-svn@170 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Tests/util/hash/Adler32Test.java
+++ Tests/util/hash/Adler32Test.java
@@ -11,9 +11,9 @@ public class Adler32Test {
 	public void basic() {
 		CryptoUtils.testWithAsciiMessage(Adler32.FUNCTION, "", "00000001");
 		CryptoUtils.testWithAsciiMessage(Adler32.FUNCTION, "Mark Adler", "13070394");
-		CryptoUtils.testWithAsciiMessage(Adler32.FUNCTION, "\000\001\002\003", "000E0007");  // Eww, octal
-		CryptoUtils.testWithAsciiMessage(Adler32.FUNCTION, "\000\001\002\003\004\005\006\007", "005C001D");
-		CryptoUtils.testWithAsciiMessage(Adler32.FUNCTION, "\000\001\002\003\004\005\006\007\010\011\012\013\014\015\016\017", "02B80079");
+		CryptoUtils.testWithHexMessage(Adler32.FUNCTION, "00010203", "000E0007");
+		CryptoUtils.testWithHexMessage(Adler32.FUNCTION, "0001020304050607", "005C001D");
+		CryptoUtils.testWithHexMessage(Adler32.FUNCTION, "000102030405060708090A0B0C0D0E0F", "02B80079");
 		CryptoUtils.testWithAsciiMessage(Adler32.FUNCTION, "AAAA", "028E0105");
 		CryptoUtils.testWithAsciiMessage(Adler32.FUNCTION, "BBBBBBBB", "09500211");
 		CryptoUtils.testWithAsciiMessage(Adler32.FUNCTION, "CCCCCCCCCCCCCCCC", "23A80431");
