701a98bcb75d744eed4a9285ba98e6d4d6c18353
Nayuki Minase <nayuki@eigenstate.org>
2008-04-26 01:24:21 +0000

    Updated tests to use the refactored helper method
    git-svn-id: file:///home/user/p79068-java-library-svn@154 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Tests/crypto/cipher/IdeaTest.java
+++ Tests/crypto/cipher/IdeaTest.java
@@ -1,33 +1,15 @@
 package crypto.cipher;
 
-import static org.junit.Assert.assertArrayEquals;
 import org.junit.Test;
 import crypto.CryptoUtils;
-import p79068.crypto.cipher.Cipherer;
 import p79068.crypto.cipher.Idea;
 
 
 public class IdeaTest {
 	
 	@Test
-	public void testIdea() {
-		test("00010002000300040005000600070008", "0000000100020003", "11fbed2b01986de5");
-	}
-	
-	
-	
-	private static void test(String key, String plaintext, String ciphertext) {
-		test(CryptoUtils.hexToBytes(key), CryptoUtils.hexToBytes(plaintext), CryptoUtils.hexToBytes(ciphertext));
-	}
-	
-	
-	private static void test(byte[] key, byte[] plaintext, byte[] ciphertext) {
-		byte[] originalplaintext = plaintext.clone();
-		Cipherer cipherer = Idea.CIPHER.newCipherer(key);
-		cipherer.encrypt(plaintext);
-		assertArrayEquals(ciphertext, plaintext);
-		cipherer.decrypt(plaintext);
-		assertArrayEquals(originalplaintext, plaintext);
+	public void testBasic() {
+		CryptoUtils.testCipher(Idea.CIPHER, "00010002000300040005000600070008", "0000000100020003", "11fbed2b01986de5");
 	}
 	
 }
