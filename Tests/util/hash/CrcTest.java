2ec15deecff514d4b843c62b46d204960d5aa0de
Nayuki Minase <nayuki@eigenstate.org>
2008-04-26 03:07:08 +0000

    Added test cases
    git-svn-id: file:///home/user/p79068-java-library-svn@172 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Tests/util/hash/CrcTest.java
+++ Tests/util/hash/CrcTest.java
@@ -10,6 +10,12 @@ public class CrcTest {
 	@Test
 	public void testCrc32() {
 		CryptoUtils.testWithAsciiMessage(Crc.CRC32_FUNCTION, "", "00000000");
+		CryptoUtils.testWithAsciiMessage(Crc.CRC32_FUNCTION, "a", "E8B7BE43");
+		CryptoUtils.testWithAsciiMessage(Crc.CRC32_FUNCTION, "abc", "352441C2");
+		CryptoUtils.testWithAsciiMessage(Crc.CRC32_FUNCTION, "message digest", "20159D7F");
+		CryptoUtils.testWithAsciiMessage(Crc.CRC32_FUNCTION, "abcdefghijklmnopqrstuvwxyz", "4C2750BD");
+		CryptoUtils.testWithAsciiMessage(Crc.CRC32_FUNCTION, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "1FC2E6d2");
+		CryptoUtils.testWithAsciiMessage(Crc.CRC32_FUNCTION, "12345678901234567890123456789012345678901234567890123456789012345678901234567890", "7CA94A72");
 		CryptoUtils.testWithHexMessage(Crc.CRC32_FUNCTION, "FFFFFFFF", "FFFFFFFF");
 		CryptoUtils.testWithHexMessage(Crc.CRC32_FUNCTION, "FFFFFFFF00", "FFFFFFFF");
 		CryptoUtils.testWithHexMessage(Crc.CRC32_FUNCTION, "FFFFFFFF0000", "FFFFFFFF");
