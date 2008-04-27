1d55f93e0983feda69e9d2708a0ad4d25209a111
Nayuki Minase <nayuki@eigenstate.org>
2008-04-27 21:12:39 +0000

    Added test suite
    git-svn-id: file:///home/user/p79068-java-library-svn@246 24ebd69f-44c7-534f-88b9-abd8d07edc6a

---
+++ Tests/datastruct/AvlTreeListTest.java
@@ -0,0 +1,12 @@
+package datastruct;
+
+import p79068.datastruct.AvlTreeList;
+
+
+public class AvlTreeListTest extends ListTest {
+	
+	protected <E> AvlTreeList<E> newList() {
+		return new AvlTreeList<E>();
+	}
+	
+}
