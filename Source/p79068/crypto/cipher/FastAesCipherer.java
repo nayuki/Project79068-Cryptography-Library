c6985a45abd8825e7a66882a00c7ae05876c7ab5
Nayuki Minase <nayuki@eigenstate.org>
2008-05-10 22:48:33 +0000

    Automatically reformatted code
    git-svn-id: file:///home/user/p79068-java-library-svn@326 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/crypto/cipher/FastAesCipherer.java
+++ Source/p79068/crypto/cipher/FastAesCipherer.java
@@ -4,155 +4,176 @@ import p79068.crypto.Zeroizer;
 import p79068.lang.*;
 
 
-final class FastAesCipherer extends RijndaelCiphererParent{
-
- private int[] encryptionKeySch; // Encryption key schedule, containing the round keys (in the order of application).
- private int[] decryptionKeySch; // Decryption key schedule, containing the round keys (in the order of application).
- private int roundCount;
-
-
- FastAesCipherer(Rijndael cipher,byte[] key){
-  super(cipher,key);
-  setKey(key);}
-
-
- public void encrypt(byte[] b,int off,int len){
-  if(cipher==null)throw new IllegalStateException("Already zeroized");
-  BoundsChecker.check(b.length,off,len);
-  if((len&0xF)!=0)throw new IllegalArgumentException("Invalid block length");
-  for(len+=off;off<len;off+=16){
-   int x0=(b[off+ 0]<<24|(b[off+ 1]&0xFF)<<16|(b[off+ 2]&0xFF)<<8|(b[off+ 3]&0xFF))^encryptionKeySch[0]; // Each variable represents a column
-   int x1=(b[off+ 4]<<24|(b[off+ 5]&0xFF)<<16|(b[off+ 6]&0xFF)<<8|(b[off+ 7]&0xFF))^encryptionKeySch[1];
-   int x2=(b[off+ 8]<<24|(b[off+ 9]&0xFF)<<16|(b[off+10]&0xFF)<<8|(b[off+11]&0xFF))^encryptionKeySch[2];
-   int x3=(b[off+12]<<24|(b[off+13]&0xFF)<<16|(b[off+14]&0xFF)<<8|(b[off+15]&0xFF))^encryptionKeySch[3];
-   for(int i=1;i<roundCount;i++){
-    int y0=mul0[x0>>>24]^mul1[x1>>>16&0xFF]^mul2[x2>>>8&0xFF]^mul3[x3&0xFF];
-    int y1=mul0[x1>>>24]^mul1[x2>>>16&0xFF]^mul2[x3>>>8&0xFF]^mul3[x0&0xFF];
-    int y2=mul0[x2>>>24]^mul1[x3>>>16&0xFF]^mul2[x0>>>8&0xFF]^mul3[x1&0xFF];
-    int y3=mul0[x3>>>24]^mul1[x0>>>16&0xFF]^mul2[x1>>>8&0xFF]^mul3[x2&0xFF];
-    x0=y0^encryptionKeySch[i<<2|0];
-    x1=y1^encryptionKeySch[i<<2|1];
-    x2=y2^encryptionKeySch[i<<2|2];
-    x3=y3^encryptionKeySch[i<<2|3];}
-   int y0=(bigsub[(x0&0xFF000000|x1&0x00FF0000)>>>16]<<16|bigsub[x2&0x0000FF00|x3&0x000000FF])^encryptionKeySch[roundCount<<2|0];
-   int y1=(bigsub[(x1&0xFF000000|x2&0x00FF0000)>>>16]<<16|bigsub[x3&0x0000FF00|x0&0x000000FF])^encryptionKeySch[roundCount<<2|1];
-   int y2=(bigsub[(x2&0xFF000000|x3&0x00FF0000)>>>16]<<16|bigsub[x0&0x0000FF00|x1&0x000000FF])^encryptionKeySch[roundCount<<2|2];
-   int y3=(bigsub[(x3&0xFF000000|x0&0x00FF0000)>>>16]<<16|bigsub[x1&0x0000FF00|x2&0x000000FF])^encryptionKeySch[roundCount<<2|3];
-   b[off+ 0]=(byte)(y0>>>24);
-   b[off+ 1]=(byte)(y0>>>16);
-   b[off+ 2]=(byte)(y0>>> 8);
-   b[off+ 3]=(byte)(y0>>> 0);
-   b[off+ 4]=(byte)(y1>>>24);
-   b[off+ 5]=(byte)(y1>>>16);
-   b[off+ 6]=(byte)(y1>>> 8);
-   b[off+ 7]=(byte)(y1>>> 0);
-   b[off+ 8]=(byte)(y2>>>24);
-   b[off+ 9]=(byte)(y2>>>16);
-   b[off+10]=(byte)(y2>>> 8);
-   b[off+11]=(byte)(y2>>> 0);
-   b[off+12]=(byte)(y3>>>24);
-   b[off+13]=(byte)(y3>>>16);
-   b[off+14]=(byte)(y3>>> 8);
-   b[off+15]=(byte)(y3>>> 0);}}
-
- public void decrypt(byte[] b,int off,int len){
-  if(cipher==null)throw new IllegalStateException("Already zeroized");
-  BoundsChecker.check(b.length,off,len);
-  if((len&0xF)!=0)throw new IllegalArgumentException("Invalid block length");
-  for(len+=off;off<len;off+=16){
-   int x0=(b[off+ 0]<<24|(b[off+ 1]&0xFF)<<16|(b[off+ 2]&0xFF)<<8|(b[off+ 3]&0xFF))^decryptionKeySch[0]; // Each variable represents a column
-   int x1=(b[off+ 4]<<24|(b[off+ 5]&0xFF)<<16|(b[off+ 6]&0xFF)<<8|(b[off+ 7]&0xFF))^decryptionKeySch[1];
-   int x2=(b[off+ 8]<<24|(b[off+ 9]&0xFF)<<16|(b[off+10]&0xFF)<<8|(b[off+11]&0xFF))^decryptionKeySch[2];
-   int x3=(b[off+12]<<24|(b[off+13]&0xFF)<<16|(b[off+14]&0xFF)<<8|(b[off+15]&0xFF))^decryptionKeySch[3];
-   for(int i=1;i<roundCount;i++){
-    int y0=mulinv0[x0>>>24]^mulinv1[x3>>>16&0xFF]^mulinv2[x2>>>8&0xFF]^mulinv3[x1&0xFF];
-    int y1=mulinv0[x1>>>24]^mulinv1[x0>>>16&0xFF]^mulinv2[x3>>>8&0xFF]^mulinv3[x2&0xFF];
-    int y2=mulinv0[x2>>>24]^mulinv1[x1>>>16&0xFF]^mulinv2[x0>>>8&0xFF]^mulinv3[x3&0xFF];
-    int y3=mulinv0[x3>>>24]^mulinv1[x2>>>16&0xFF]^mulinv2[x1>>>8&0xFF]^mulinv3[x0&0xFF];
-    x0=y0^decryptionKeySch[i<<2|0];
-    x1=y1^decryptionKeySch[i<<2|1];
-    x2=y2^decryptionKeySch[i<<2|2];
-    x3=y3^decryptionKeySch[i<<2|3];}
-   int y0=(bigsubinv[(x0&0xFF000000|x3&0x00FF0000)>>>16]<<16|bigsubinv[x2&0x0000FF00|x1&0x000000FF])^decryptionKeySch[roundCount<<2|0];
-   int y1=(bigsubinv[(x1&0xFF000000|x0&0x00FF0000)>>>16]<<16|bigsubinv[x3&0x0000FF00|x2&0x000000FF])^decryptionKeySch[roundCount<<2|1];
-   int y2=(bigsubinv[(x2&0xFF000000|x1&0x00FF0000)>>>16]<<16|bigsubinv[x0&0x0000FF00|x3&0x000000FF])^decryptionKeySch[roundCount<<2|2];
-   int y3=(bigsubinv[(x3&0xFF000000|x2&0x00FF0000)>>>16]<<16|bigsubinv[x1&0x0000FF00|x0&0x000000FF])^decryptionKeySch[roundCount<<2|3];
-   b[off+ 0]=(byte)(y0>>>24);
-   b[off+ 1]=(byte)(y0>>>16);
-   b[off+ 2]=(byte)(y0>>> 8);
-   b[off+ 3]=(byte)(y0>>> 0);
-   b[off+ 4]=(byte)(y1>>>24);
-   b[off+ 5]=(byte)(y1>>>16);
-   b[off+ 6]=(byte)(y1>>> 8);
-   b[off+ 7]=(byte)(y1>>> 0);
-   b[off+ 8]=(byte)(y2>>>24);
-   b[off+ 9]=(byte)(y2>>>16);
-   b[off+10]=(byte)(y2>>> 8);
-   b[off+11]=(byte)(y2>>> 0);
-   b[off+12]=(byte)(y3>>>24);
-   b[off+13]=(byte)(y3>>>16);
-   b[off+14]=(byte)(y3>>> 8);
-   b[off+15]=(byte)(y3>>> 0);}}
-
-
- public void zeroize(){
-  if(cipher==null)return;
-  Zeroizer.clear(encryptionKeySch);
-  Zeroizer.clear(decryptionKeySch);
-  encryptionKeySch=null;
-  decryptionKeySch=null;
-  super.zeroize();}
-
-
- private void setKey(byte[] key){
-  if(key.length%4!=0||key.length==0)throw new IllegalArgumentException("Invalid key length");
-  int nk=key.length/4; // Number of 32-bit blocks in the key
-  roundCount=Math.max(nk,4)+6;
-  encryptionKeySch=expandKey(key,4);
-  decryptionKeySch=new int[encryptionKeySch.length];
-  for(int i=0;i<roundCount+1;i++){
-   if(i==0||i==roundCount)System.arraycopy(encryptionKeySch,(roundCount-i)*4,decryptionKeySch,i*4,4);
-   else{
-    for(int j=0;j<4;j++){
-     decryptionKeySch[i*4+j]=
-       mulinv0[sub[encryptionKeySch[(roundCount-i)*4+j]>>>24&0xFF]&0xFF]
-      ^mulinv1[sub[encryptionKeySch[(roundCount-i)*4+j]>>>16&0xFF]&0xFF]
-      ^mulinv2[sub[encryptionKeySch[(roundCount-i)*4+j]>>> 8&0xFF]&0xFF]
-      ^mulinv3[sub[encryptionKeySch[(roundCount-i)*4+j]>>> 0&0xFF]&0xFF];}}}}
-
-
- private static int[] bigsub,bigsubinv; // Substitutes 2 bytes in parallel.
- private static int[] mul0,mul1,mul2,mul3;
- private static int[] mulinv0,mulinv1,mulinv2,mulinv3;
-
- static{
-  initBigSBox();
-  initMultiplyTable();}
-
- private static void initBigSBox(){
-  bigsub=new int[65536];
-  bigsubinv=new int[65536];
-  for(int i=0;i<65536;i++){
-   bigsub[i]=(sub[i>>>8]&0xFF)<<8|(sub[i&0xFF]&0xFF);
-   bigsubinv[i]=(subinv[i>>>8]&0xFF)<<8|(subinv[i&0xFF]&0xFF);}}
-
- private static void initMultiplyTable(){
-  mul0=new int[256];
-  mul1=new int[256];
-  mul2=new int[256];
-  mul3=new int[256];
-  mulinv0=new int[256];
-  mulinv1=new int[256];
-  mulinv2=new int[256];
-  mulinv3=new int[256];
-  for(int i=0;i<256;i++){
-   int j=sub[i]&0xFF;
-   mul0[i]=multiply(j,0x02)<<24|multiply(j,0x01)<<16|multiply(j,0x01)<<8|multiply(j,0x03);
-   mul1[i]=mul0[i]<<24|mul0[i]>>> 8;
-   mul2[i]=mul0[i]<<16|mul0[i]>>>16;
-   mul3[i]=mul0[i]<< 8|mul0[i]>>>24;
-   j=subinv[i]&0xFF;
-   mulinv0[i]=multiply(j,0x0E)<<24|multiply(j,0x09)<<16|multiply(j,0x0D)<<8|multiply(j,0x0B);
-   mulinv1[i]=mulinv0[i]<<24|mulinv0[i]>>> 8;
-   mulinv2[i]=mulinv0[i]<<16|mulinv0[i]>>>16;
-   mulinv3[i]=mulinv0[i]<< 8|mulinv0[i]>>>24;}}}
+final class FastAesCipherer extends RijndaelCiphererParent {
+	
+	private int[] encryptionKeySch; // Encryption key schedule, containing the round keys (in the order of application).
+	private int[] decryptionKeySch; // Decryption key schedule, containing the round keys (in the order of application).
+	private int roundCount;
+	
+	
+	FastAesCipherer(Rijndael cipher, byte[] key) {
+		super(cipher, key);
+		setKey(key);
+	}
+	
+	
+	public void encrypt(byte[] b, int off, int len) {
+		if (cipher == null)
+			throw new IllegalStateException("Already zeroized");
+		BoundsChecker.check(b.length, off, len);
+		if ((len & 0xF) != 0)
+			throw new IllegalArgumentException("Invalid block length");
+		for (len += off; off < len; off += 16) {
+			int x0 = (b[off + 0] << 24 | (b[off + 1] & 0xFF) << 16 | (b[off + 2] & 0xFF) << 8 | (b[off + 3] & 0xFF)) ^ encryptionKeySch[0]; // Each variable represents a column
+			int x1 = (b[off + 4] << 24 | (b[off + 5] & 0xFF) << 16 | (b[off + 6] & 0xFF) << 8 | (b[off + 7] & 0xFF)) ^ encryptionKeySch[1];
+			int x2 = (b[off + 8] << 24 | (b[off + 9] & 0xFF) << 16 | (b[off + 10] & 0xFF) << 8 | (b[off + 11] & 0xFF)) ^ encryptionKeySch[2];
+			int x3 = (b[off + 12] << 24 | (b[off + 13] & 0xFF) << 16 | (b[off + 14] & 0xFF) << 8 | (b[off + 15] & 0xFF)) ^ encryptionKeySch[3];
+			for (int i = 1; i < roundCount; i++) {
+				int y0 = mul0[x0 >>> 24] ^ mul1[x1 >>> 16 & 0xFF] ^ mul2[x2 >>> 8 & 0xFF] ^ mul3[x3 & 0xFF];
+				int y1 = mul0[x1 >>> 24] ^ mul1[x2 >>> 16 & 0xFF] ^ mul2[x3 >>> 8 & 0xFF] ^ mul3[x0 & 0xFF];
+				int y2 = mul0[x2 >>> 24] ^ mul1[x3 >>> 16 & 0xFF] ^ mul2[x0 >>> 8 & 0xFF] ^ mul3[x1 & 0xFF];
+				int y3 = mul0[x3 >>> 24] ^ mul1[x0 >>> 16 & 0xFF] ^ mul2[x1 >>> 8 & 0xFF] ^ mul3[x2 & 0xFF];
+				x0 = y0 ^ encryptionKeySch[i << 2 | 0];
+				x1 = y1 ^ encryptionKeySch[i << 2 | 1];
+				x2 = y2 ^ encryptionKeySch[i << 2 | 2];
+				x3 = y3 ^ encryptionKeySch[i << 2 | 3];
+			}
+			int y0 = (bigsub[(x0 & 0xFF000000 | x1 & 0x00FF0000) >>> 16] << 16 | bigsub[x2 & 0x0000FF00 | x3 & 0x000000FF]) ^ encryptionKeySch[roundCount << 2 | 0];
+			int y1 = (bigsub[(x1 & 0xFF000000 | x2 & 0x00FF0000) >>> 16] << 16 | bigsub[x3 & 0x0000FF00 | x0 & 0x000000FF]) ^ encryptionKeySch[roundCount << 2 | 1];
+			int y2 = (bigsub[(x2 & 0xFF000000 | x3 & 0x00FF0000) >>> 16] << 16 | bigsub[x0 & 0x0000FF00 | x1 & 0x000000FF]) ^ encryptionKeySch[roundCount << 2 | 2];
+			int y3 = (bigsub[(x3 & 0xFF000000 | x0 & 0x00FF0000) >>> 16] << 16 | bigsub[x1 & 0x0000FF00 | x2 & 0x000000FF]) ^ encryptionKeySch[roundCount << 2 | 3];
+			b[off + 0] = (byte)(y0 >>> 24);
+			b[off + 1] = (byte)(y0 >>> 16);
+			b[off + 2] = (byte)(y0 >>> 8);
+			b[off + 3] = (byte)(y0 >>> 0);
+			b[off + 4] = (byte)(y1 >>> 24);
+			b[off + 5] = (byte)(y1 >>> 16);
+			b[off + 6] = (byte)(y1 >>> 8);
+			b[off + 7] = (byte)(y1 >>> 0);
+			b[off + 8] = (byte)(y2 >>> 24);
+			b[off + 9] = (byte)(y2 >>> 16);
+			b[off + 10] = (byte)(y2 >>> 8);
+			b[off + 11] = (byte)(y2 >>> 0);
+			b[off + 12] = (byte)(y3 >>> 24);
+			b[off + 13] = (byte)(y3 >>> 16);
+			b[off + 14] = (byte)(y3 >>> 8);
+			b[off + 15] = (byte)(y3 >>> 0);
+		}
+	}
+	
+	public void decrypt(byte[] b, int off, int len) {
+		if (cipher == null)
+			throw new IllegalStateException("Already zeroized");
+		BoundsChecker.check(b.length, off, len);
+		if ((len & 0xF) != 0)
+			throw new IllegalArgumentException("Invalid block length");
+		for (len += off; off < len; off += 16) {
+			int x0 = (b[off + 0] << 24 | (b[off + 1] & 0xFF) << 16 | (b[off + 2] & 0xFF) << 8 | (b[off + 3] & 0xFF)) ^ decryptionKeySch[0]; // Each variable represents a column
+			int x1 = (b[off + 4] << 24 | (b[off + 5] & 0xFF) << 16 | (b[off + 6] & 0xFF) << 8 | (b[off + 7] & 0xFF)) ^ decryptionKeySch[1];
+			int x2 = (b[off + 8] << 24 | (b[off + 9] & 0xFF) << 16 | (b[off + 10] & 0xFF) << 8 | (b[off + 11] & 0xFF)) ^ decryptionKeySch[2];
+			int x3 = (b[off + 12] << 24 | (b[off + 13] & 0xFF) << 16 | (b[off + 14] & 0xFF) << 8 | (b[off + 15] & 0xFF)) ^ decryptionKeySch[3];
+			for (int i = 1; i < roundCount; i++) {
+				int y0 = mulinv0[x0 >>> 24] ^ mulinv1[x3 >>> 16 & 0xFF] ^ mulinv2[x2 >>> 8 & 0xFF] ^ mulinv3[x1 & 0xFF];
+				int y1 = mulinv0[x1 >>> 24] ^ mulinv1[x0 >>> 16 & 0xFF] ^ mulinv2[x3 >>> 8 & 0xFF] ^ mulinv3[x2 & 0xFF];
+				int y2 = mulinv0[x2 >>> 24] ^ mulinv1[x1 >>> 16 & 0xFF] ^ mulinv2[x0 >>> 8 & 0xFF] ^ mulinv3[x3 & 0xFF];
+				int y3 = mulinv0[x3 >>> 24] ^ mulinv1[x2 >>> 16 & 0xFF] ^ mulinv2[x1 >>> 8 & 0xFF] ^ mulinv3[x0 & 0xFF];
+				x0 = y0 ^ decryptionKeySch[i << 2 | 0];
+				x1 = y1 ^ decryptionKeySch[i << 2 | 1];
+				x2 = y2 ^ decryptionKeySch[i << 2 | 2];
+				x3 = y3 ^ decryptionKeySch[i << 2 | 3];
+			}
+			int y0 = (bigsubinv[(x0 & 0xFF000000 | x3 & 0x00FF0000) >>> 16] << 16 | bigsubinv[x2 & 0x0000FF00 | x1 & 0x000000FF]) ^ decryptionKeySch[roundCount << 2 | 0];
+			int y1 = (bigsubinv[(x1 & 0xFF000000 | x0 & 0x00FF0000) >>> 16] << 16 | bigsubinv[x3 & 0x0000FF00 | x2 & 0x000000FF]) ^ decryptionKeySch[roundCount << 2 | 1];
+			int y2 = (bigsubinv[(x2 & 0xFF000000 | x1 & 0x00FF0000) >>> 16] << 16 | bigsubinv[x0 & 0x0000FF00 | x3 & 0x000000FF]) ^ decryptionKeySch[roundCount << 2 | 2];
+			int y3 = (bigsubinv[(x3 & 0xFF000000 | x2 & 0x00FF0000) >>> 16] << 16 | bigsubinv[x1 & 0x0000FF00 | x0 & 0x000000FF]) ^ decryptionKeySch[roundCount << 2 | 3];
+			b[off + 0] = (byte)(y0 >>> 24);
+			b[off + 1] = (byte)(y0 >>> 16);
+			b[off + 2] = (byte)(y0 >>> 8);
+			b[off + 3] = (byte)(y0 >>> 0);
+			b[off + 4] = (byte)(y1 >>> 24);
+			b[off + 5] = (byte)(y1 >>> 16);
+			b[off + 6] = (byte)(y1 >>> 8);
+			b[off + 7] = (byte)(y1 >>> 0);
+			b[off + 8] = (byte)(y2 >>> 24);
+			b[off + 9] = (byte)(y2 >>> 16);
+			b[off + 10] = (byte)(y2 >>> 8);
+			b[off + 11] = (byte)(y2 >>> 0);
+			b[off + 12] = (byte)(y3 >>> 24);
+			b[off + 13] = (byte)(y3 >>> 16);
+			b[off + 14] = (byte)(y3 >>> 8);
+			b[off + 15] = (byte)(y3 >>> 0);
+		}
+	}
+	
+	
+	public void zeroize() {
+		if (cipher == null)
+			return;
+		Zeroizer.clear(encryptionKeySch);
+		Zeroizer.clear(decryptionKeySch);
+		encryptionKeySch = null;
+		decryptionKeySch = null;
+		super.zeroize();
+	}
+	
+	
+	private void setKey(byte[] key) {
+		if (key.length % 4 != 0 || key.length == 0)
+			throw new IllegalArgumentException("Invalid key length");
+		int nk = key.length / 4; // Number of 32-bit blocks in the key
+		roundCount = Math.max(nk, 4) + 6;
+		encryptionKeySch = expandKey(key, 4);
+		decryptionKeySch = new int[encryptionKeySch.length];
+		for (int i = 0; i < roundCount + 1; i++) {
+			if (i == 0 || i == roundCount)
+				System.arraycopy(encryptionKeySch, (roundCount - i) * 4, decryptionKeySch, i * 4, 4);
+			else {
+				for (int j = 0; j < 4; j++) {
+					decryptionKeySch[i * 4 + j] = mulinv0[sub[encryptionKeySch[(roundCount - i) * 4 + j] >>> 24 & 0xFF] & 0xFF] ^ mulinv1[sub[encryptionKeySch[(roundCount - i) * 4 + j] >>> 16 & 0xFF] & 0xFF] ^ mulinv2[sub[encryptionKeySch[(roundCount - i) * 4 + j] >>> 8 & 0xFF] & 0xFF] ^ mulinv3[sub[encryptionKeySch[(roundCount - i) * 4 + j] >>> 0 & 0xFF] & 0xFF];
+				}
+			}
+		}
+	}
+	
+	
+	private static int[] bigsub, bigsubinv; // Substitutes 2 bytes in parallel.
+	private static int[] mul0, mul1, mul2, mul3;
+	private static int[] mulinv0, mulinv1, mulinv2, mulinv3;
+	
+	static {
+		initBigSBox();
+		initMultiplyTable();
+	}
+	
+	private static void initBigSBox() {
+		bigsub = new int[65536];
+		bigsubinv = new int[65536];
+		for (int i = 0; i < 65536; i++) {
+			bigsub[i] = (sub[i >>> 8] & 0xFF) << 8 | (sub[i & 0xFF] & 0xFF);
+			bigsubinv[i] = (subinv[i >>> 8] & 0xFF) << 8 | (subinv[i & 0xFF] & 0xFF);
+		}
+	}
+	
+	private static void initMultiplyTable() {
+		mul0 = new int[256];
+		mul1 = new int[256];
+		mul2 = new int[256];
+		mul3 = new int[256];
+		mulinv0 = new int[256];
+		mulinv1 = new int[256];
+		mulinv2 = new int[256];
+		mulinv3 = new int[256];
+		for (int i = 0; i < 256; i++) {
+			int j = sub[i] & 0xFF;
+			mul0[i] = multiply(j, 0x02) << 24 | multiply(j, 0x01) << 16 | multiply(j, 0x01) << 8 | multiply(j, 0x03);
+			mul1[i] = mul0[i] << 24 | mul0[i] >>> 8;
+			mul2[i] = mul0[i] << 16 | mul0[i] >>> 16;
+			mul3[i] = mul0[i] << 8 | mul0[i] >>> 24;
+			j = subinv[i] & 0xFF;
+			mulinv0[i] = multiply(j, 0x0E) << 24 | multiply(j, 0x09) << 16 | multiply(j, 0x0D) << 8 | multiply(j, 0x0B);
+			mulinv1[i] = mulinv0[i] << 24 | mulinv0[i] >>> 8;
+			mulinv2[i] = mulinv0[i] << 16 | mulinv0[i] >>> 16;
+			mulinv3[i] = mulinv0[i] << 8 | mulinv0[i] >>> 24;
+		}
+	}
+}
--- Source/p79068/crypto/cipher/FastWhirlpoolCipherer.java
+++ Source/p79068/crypto/cipher/FastWhirlpoolCipherer.java
@@ -9,154 +9,192 @@ import p79068.crypto.Zeroizer;
 import p79068.lang.*;
 
 
-final class FastWhirlpoolCipherer extends Cipherer{
+final class FastWhirlpoolCipherer extends Cipherer {
+	
+	private long[][] rcon;
+	private long[][] mul;
+	
+	private long[][] keySchedule;
+	
+	
+	FastWhirlpoolCipherer(WhirlpoolCipher cipher, byte[] key) {
+		super(cipher, key);
+		rcon = RCON;
+		mul = MUL;
+		setKey(key);
+	}
+	
+	
+	public void encrypt(byte[] b, int off, int len) {
+		if (cipher == null)
+			throw new IllegalStateException("Already zeroized");
+		BoundsChecker.check(b.length, off, len);
+		if ((len & 0x3F) != 0)
+			throw new IllegalArgumentException("Invalid block length");
+		long[] tempmsg = new long[8];
+		long[] temp = new long[8];
+		for (len += off; off < len; off += 64) {
+			toInt64sBigEndian(b, off, tempmsg);
+			w(tempmsg, temp);
+			toBytesBigEndian(tempmsg, b, off);
+		}
+	}
+	
+	public void decrypt(byte[] b, int off, int len) {
+		if (cipher == null)
+			throw new IllegalStateException("Already zeroized");
+		BoundsChecker.check(b.length, off, len);
+		if ((len & 0x3F) != 0)
+			throw new IllegalArgumentException("Invalid block length");
+		long[] tempmsg = new long[8];
+		long[] temp = new long[8];
+		for (len += off; off < len; off += 64) {
+			toInt64sBigEndian(b, off, tempmsg);
+			//   wInverse(tempmsg,temp);
+			toBytesBigEndian(tempmsg, b, off);
+		}
+		throw new RuntimeException("Not implemented yet");
+	}
+	
+	
+	public void zeroize() {
+		if (cipher == null)
+			return;
+		for (int i = 0; i < keySchedule.length; i++) {
+			Zeroizer.clear(keySchedule[i]);
+			keySchedule[i] = null;
+		}
+		keySchedule = null;
+		super.zeroize();
+	}
+	
+	
+	private void setKey(byte[] key) {
+		if (key.length != 64)
+			throw new AssertionError();
+		keySchedule = new long[rcon.length + 1][];
+		keySchedule[0] = new long[8];
+		toInt64sBigEndian(key, 0, keySchedule[0]);
+		long[] temp = new long[64];
+		for (int i = 1; i < keySchedule.length; i++) {
+			keySchedule[i] = keySchedule[i - 1].clone();
+			rho(keySchedule[i], rcon[i - 1], temp);
+		}
+	}
+	
+	
+	private void w(long[] block, long[] temp) { // The internal block cipher. Overwrites block and temp.
+		for (int i = 0; i < 8; i++)
+			block[i] ^= keySchedule[0][i]; // Sigma
+		for (int i = 0; i < rcon.length; i++)
+			rho(block, keySchedule[i], temp);
+	}
+	
+	private void rho(long[] block, long[] key, long[] temp) { // The round function. Overwrites block and temp.
+		for (int i = 0; i < 8; i++)
+			temp[i] = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++)
+				temp[(i + j) & 7] ^= mul[j][(int)(block[i] >>> ((j ^ 7) << 3)) & 0xFF];
+		}
+		for (int i = 0; i < 8; i++)
+			block[i] = temp[i] ^ key[i];
+	} // Sigma
+	
 
- private long[][] rcon;
- private long[][] mul;
-
- private long[][] keySchedule;
-
-
- FastWhirlpoolCipherer(WhirlpoolCipher cipher,byte[] key){
-  super(cipher,key);
-  rcon=RCON;
-  mul=MUL;
-  setKey(key);}
-
-
- public void encrypt(byte[] b,int off,int len){
-  if(cipher==null)throw new IllegalStateException("Already zeroized");
-  BoundsChecker.check(b.length,off,len);
-  if((len&0x3F)!=0)throw new IllegalArgumentException("Invalid block length");
-  long[] tempmsg=new long[8];
-  long[] temp=new long[8];
-  for(len+=off;off<len;off+=64){
-   toInt64sBigEndian(b,off,tempmsg);
-   w(tempmsg,temp);
-   toBytesBigEndian(tempmsg,b,off);}}
-
- public void decrypt(byte[] b,int off,int len){
-  if(cipher==null)throw new IllegalStateException("Already zeroized");
-  BoundsChecker.check(b.length,off,len);
-  if((len&0x3F)!=0)throw new IllegalArgumentException("Invalid block length");
-  long[] tempmsg=new long[8];
-  long[] temp=new long[8];
-  for(len+=off;off<len;off+=64){
-   toInt64sBigEndian(b,off,tempmsg);
-//   wInverse(tempmsg,temp);
-   toBytesBigEndian(tempmsg,b,off);}
-  throw new RuntimeException("Not implemented yet");}
-
-
- public void zeroize(){
-  if(cipher==null)return;
-  for(int i=0;i<keySchedule.length;i++){
-   Zeroizer.clear(keySchedule[i]);
-   keySchedule[i]=null;}
-  keySchedule=null;
-  super.zeroize();}
-
-
- private void setKey(byte[] key){
-  if(key.length!=64)throw new AssertionError();
-  keySchedule=new long[rcon.length+1][];
-  keySchedule[0]=new long[8];
-  toInt64sBigEndian(key,0,keySchedule[0]);
-  long[] temp=new long[64];
-  for(int i=1;i<keySchedule.length;i++){
-   keySchedule[i]=keySchedule[i-1].clone();
-   rho(keySchedule[i],rcon[i-1],temp);}}
-
-
- private void w(long[] block,long[] temp){ // The internal block cipher. Overwrites block and temp.
-  for(int i=0;i<8;i++)block[i]^=keySchedule[0][i]; // Sigma
-  for(int i=0;i<rcon.length;i++)rho(block,keySchedule[i],temp);}
-
- private void rho(long[] block,long[] key,long[] temp){ // The round function. Overwrites block and temp.
-  for(int i=0;i<8;i++)temp[i]=0;
-  for(int i=0;i<8;i++){
-   for(int j=0;j<8;j++)temp[(i+j)&7]^=mul[j][(int)(block[i]>>>((j^7)<<3))&0xFF];}
-  for(int i=0;i<8;i++)block[i]=temp[i]^key[i];} // Sigma
-
-
- private static void toInt64sBigEndian(byte[] in,int off,long[] out){
-  for(int i=0;i<out.length;i++,off+=8){
-   out[i]=
-     (in[off+0]&0xFFL)<<56
-    |(in[off+1]&0xFFL)<<48
-    |(in[off+2]&0xFFL)<<40
-    |(in[off+3]&0xFFL)<<32
-    |(in[off+4]&0xFFL)<<24
-    |(in[off+5]&0xFFL)<<16
-    |(in[off+6]&0xFFL)<< 8
-    |(in[off+7]&0xFFL)<< 0;}}
-
- private static void toBytesBigEndian(long[] in,byte[] out,int off){
-  for(int i=0;i<in.length;i++,off+=8){
-   out[off+0]=(byte)(in[i]>>>56);
-   out[off+1]=(byte)(in[i]>>>48);
-   out[off+2]=(byte)(in[i]>>>40);
-   out[off+3]=(byte)(in[i]>>>32);
-   out[off+4]=(byte)(in[i]>>>24);
-   out[off+5]=(byte)(in[i]>>>16);
-   out[off+6]=(byte)(in[i]>>> 8);
-   out[off+7]=(byte)(in[i]>>> 0);}}
-
-
- private static final int ROUNDS=10;
-
- private static int[] exp; // exp[i] = pow(0x02,i) in GF(2^8)/0x11D.
- private static int[] log; // These are only used in class initialization.
-
- private static int[] SUB;
- private static long[][] MUL;
- private static long[][] RCON;
-
- static{
-  initExpLogTables();
-  initSBox();
-  initMultiplyTable(new int[]{0x01,0x01,0x04,0x01,0x08,0x05,0x02,0x09});
-  initRoundConstant();}
-
- private static void initExpLogTables(){
-  exp=new int[255];
-  log=new int[256];
-  exp[0]=1;
-  log[0]=Integer.MIN_VALUE;
-  log[1]=0;
-  for(int i=1;i<exp.length;i++){
-   exp[i]=exp[i-1]<<1; // Multiply by 0x02
-   if((exp[i]&0x100)!=0)exp[i]^=0x11D; // Modulo by 0x11D in GF(2)
-   log[exp[i]]=i;}}
-
- private static void initSBox(){
-  int[] e={0x1,0xB,0x9,0xC,0xD,0x6,0xF,0x3,0xE,0x8,0x7,0x4,0xA,0x2,0x5,0x0}; // The E mini-box
-  int[] r={0x7,0xC,0xB,0xD,0xE,0x4,0x9,0xF,0x6,0x3,0x8,0xA,0x2,0x5,0x1,0x0}; // The R mini-box
-  int[] einv=new int[16]; // The inverse of E
-  for(int i=0;i<e.length;i++)einv[e[i]]=i;
-  SUB=new int[256];
-  for(int i=0;i<SUB.length;i++){
-   int left=e[i>>>4];
-   int right=einv[i&0xF];
-   int temp=r[left^right];
-   SUB[i]=e[left^temp]<<4|einv[right^temp];}}
-
- private static void initMultiplyTable(int[] c){
-  MUL=new long[8][256];
-  for(int i=0;i<256;i++){
-   long vector=0;
-   for(int j=0;j<8;j++)vector|=(long)multiply(SUB[i],c[j])<<((7-j)*8);
-   for(int j=0;j<8;j++)MUL[j][i]=rotateRight(vector,j*8);}}
-
- private static void initRoundConstant(){
-  RCON=new long[ROUNDS][8];
-  for(int i=0;i<RCON.length;i++){
-   for(int j=0;j<8;j++)RCON[i][0]|=(long)SUB[8*i+j]<<((7-j)*8);
-   for(int j=1;j<8;j++)RCON[i][j]=0;}}
-
- private static int multiply(int x,int y){
-  if(x==0||y==0)return 0;
-  return exp[(log[x]+log[y])%255];}
-
- private static long rotateRight(long x,int rotate){
-  return x<<(64-rotate)|x>>>rotate;}}
+	private static void toInt64sBigEndian(byte[] in, int off, long[] out) {
+		for (int i = 0; i < out.length; i++, off += 8) {
+			out[i] = (in[off + 0] & 0xFFL) << 56 | (in[off + 1] & 0xFFL) << 48 | (in[off + 2] & 0xFFL) << 40 | (in[off + 3] & 0xFFL) << 32 | (in[off + 4] & 0xFFL) << 24 | (in[off + 5] & 0xFFL) << 16 | (in[off + 6] & 0xFFL) << 8 | (in[off + 7] & 0xFFL) << 0;
+		}
+	}
+	
+	private static void toBytesBigEndian(long[] in, byte[] out, int off) {
+		for (int i = 0; i < in.length; i++, off += 8) {
+			out[off + 0] = (byte)(in[i] >>> 56);
+			out[off + 1] = (byte)(in[i] >>> 48);
+			out[off + 2] = (byte)(in[i] >>> 40);
+			out[off + 3] = (byte)(in[i] >>> 32);
+			out[off + 4] = (byte)(in[i] >>> 24);
+			out[off + 5] = (byte)(in[i] >>> 16);
+			out[off + 6] = (byte)(in[i] >>> 8);
+			out[off + 7] = (byte)(in[i] >>> 0);
+		}
+	}
+	
+	
+	private static final int ROUNDS = 10;
+	
+	private static int[] exp; // exp[i] = pow(0x02,i) in GF(2^8)/0x11D.
+	private static int[] log; // These are only used in class initialization.
+	
+	private static int[] SUB;
+	private static long[][] MUL;
+	private static long[][] RCON;
+	
+	static {
+		initExpLogTables();
+		initSBox();
+		initMultiplyTable(new int[]{0x01, 0x01, 0x04, 0x01, 0x08, 0x05, 0x02, 0x09});
+		initRoundConstant();
+	}
+	
+	private static void initExpLogTables() {
+		exp = new int[255];
+		log = new int[256];
+		exp[0] = 1;
+		log[0] = Integer.MIN_VALUE;
+		log[1] = 0;
+		for (int i = 1; i < exp.length; i++) {
+			exp[i] = exp[i - 1] << 1; // Multiply by 0x02
+			if ((exp[i] & 0x100) != 0)
+				exp[i] ^= 0x11D; // Modulo by 0x11D in GF(2)
+			log[exp[i]] = i;
+		}
+	}
+	
+	private static void initSBox() {
+		int[] e = {0x1, 0xB, 0x9, 0xC, 0xD, 0x6, 0xF, 0x3, 0xE, 0x8, 0x7, 0x4, 0xA, 0x2, 0x5, 0x0}; // The E mini-box
+		int[] r = {0x7, 0xC, 0xB, 0xD, 0xE, 0x4, 0x9, 0xF, 0x6, 0x3, 0x8, 0xA, 0x2, 0x5, 0x1, 0x0}; // The R mini-box
+		int[] einv = new int[16]; // The inverse of E
+		for (int i = 0; i < e.length; i++)
+			einv[e[i]] = i;
+		SUB = new int[256];
+		for (int i = 0; i < SUB.length; i++) {
+			int left = e[i >>> 4];
+			int right = einv[i & 0xF];
+			int temp = r[left ^ right];
+			SUB[i] = e[left ^ temp] << 4 | einv[right ^ temp];
+		}
+	}
+	
+	private static void initMultiplyTable(int[] c) {
+		MUL = new long[8][256];
+		for (int i = 0; i < 256; i++) {
+			long vector = 0;
+			for (int j = 0; j < 8; j++)
+				vector |= (long)multiply(SUB[i], c[j]) << ((7 - j) * 8);
+			for (int j = 0; j < 8; j++)
+				MUL[j][i] = rotateRight(vector, j * 8);
+		}
+	}
+	
+	private static void initRoundConstant() {
+		RCON = new long[ROUNDS][8];
+		for (int i = 0; i < RCON.length; i++) {
+			for (int j = 0; j < 8; j++)
+				RCON[i][0] |= (long)SUB[8 * i + j] << ((7 - j) * 8);
+			for (int j = 1; j < 8; j++)
+				RCON[i][j] = 0;
+		}
+	}
+	
+	private static int multiply(int x, int y) {
+		if (x == 0 || y == 0)
+			return 0;
+		return exp[(log[x] + log[y]) % 255];
+	}
+	
+	private static long rotateRight(long x, int rotate) {
+		return x << (64 - rotate) | x >>> rotate;
+	}
+}
