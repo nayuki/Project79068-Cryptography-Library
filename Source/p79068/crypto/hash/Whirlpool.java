107060715f9c6b723f2161f76db21ac05bcfe03b
Nayuki Minase <nayuki@eigenstate.org>
2008-05-10 20:44:12 +0000

    Added constants
    git-svn-id: file:///home/user/p79068-java-library-svn@300 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/crypto/hash/Whirlpool.java
+++ Source/p79068/crypto/hash/Whirlpool.java
@@ -63,4 +63,68 @@ public class Whirlpool extends BlockHashFunction {
 	
 	private Whirlpool() {}
 	
+	
+	
+	private static final int ROUNDS = 10;
+	
+	private static final byte[] SUB;
+	
+	private static final byte[][] MUL;
+	
+	private static final byte[][] RCON;
+	
+	
+	static {
+		if (ROUNDS < 1 || ROUNDS > 32)
+			throw new AssertionError("Invalid number of rounds");
+		SUB = makeSub();
+		RCON = makeRcon(SUB);
+		MUL = makeMul();
+	}
+	
+	
+	// Identical to the one in Whirlpool-1
+	private static byte[] makeSub() {
+		byte[] sub = new byte[256];
+		
+		int[] e = {0x1, 0xB, 0x9, 0xC, 0xD, 0x6, 0xF, 0x3, 0xE, 0x8, 0x7, 0x4, 0xA, 0x2, 0x5, 0x0};  // The E mini-box
+		int[] r = {0x7, 0xC, 0xB, 0xD, 0xE, 0x4, 0x9, 0xF, 0x6, 0x3, 0x8, 0xA, 0x2, 0x5, 0x1, 0x0};  // The R mini-box
+		
+		int[] einv = new int[16];  // The inverse of E
+		for (int i = 0; i < e.length; i++)
+			einv[e[i]] = i;
+		
+		for (int i = 0; i < sub.length; i++) {
+			int left = e[i >>> 4];
+			int right = einv[i & 0xF];
+			int temp = r[left ^ right];
+			sub[i] = (byte)(e[left ^ temp] << 4 | einv[right ^ temp]);
+		}
+		
+		return sub;
+	}
+	
+	
+	private static byte[][] makeRcon(byte[] sub) {
+		byte[][] rcon = new byte[ROUNDS][64];
+		for (int i = 0; i < rcon.length; i++) {
+			for (int j = 0; j < 8; j++)  // The leading 8 bytes (top row) are taken from the S-box
+				rcon[i][j] = sub[8 * i + j];
+			for (int j = 8; j < 64; j++)  // The remaining 7 rows are zero
+				rcon[i][j] = 0;
+		}
+		return rcon;
+	}
+	
+	
+	private static byte[][] makeMul() {
+		byte[][] mul = new byte[8][256];
+		int[] c = {0x01, 0x05, 0x09, 0x08, 0x05, 0x01, 0x03, 0x01};
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < 256; j++)
+				mul[i][j] = (byte)WhirlpoolUtil.multiply(j, c[i]);
+		}
+		return mul;
+	}
+	
 }
