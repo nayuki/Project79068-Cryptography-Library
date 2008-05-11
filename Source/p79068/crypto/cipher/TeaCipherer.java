d72601ce2c318c00996456ef3821345fbb971207
Nayuki Minase <nayuki@eigenstate.org>
2008-05-11 15:01:01 +0000

    Added comments; made minor edits
    git-svn-id: file:///home/user/p79068-java-library-svn@343 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/crypto/cipher/TeaCipherer.java
+++ Source/p79068/crypto/cipher/TeaCipherer.java
@@ -18,8 +18,12 @@ final class TeaCipherer extends Cipherer {
 	TeaCipherer(Tea cipher, byte[] key) {
 		super(cipher, key);
 		int32Key = new int[4];
-		for (int i = 0; i < int32Key.length; i++)
-			int32Key[i] = key[i * 4] << 24 | (key[i * 4 + 1] & 0xFF) << 16 | (key[i * 4 + 2] & 0xFF) << 8 | (key[i * 4 + 3] & 0xFF);
+		for (int i = 0; i < int32Key.length; i++) {
+			int32Key[i] = (key[i * 4 + 0] & 0xFF) << 24
+			            | (key[i * 4 + 1] & 0xFF) << 16
+			            | (key[i * 4 + 2] & 0xFF) <<  8
+			            | (key[i * 4 + 3] & 0xFF) <<  0;
+		}
 	}
 	
 	
@@ -28,14 +32,18 @@ final class TeaCipherer extends Cipherer {
 		if (cipher == null)
 			throw new IllegalStateException("Already zeroized");
 		BoundsChecker.check(b.length, off, len);
-		if ((len & 7) != 0)
+		if (len % 8 != 0)
 			throw new IllegalArgumentException("Invalid block length");
 		
 		int k0 = int32Key[0];
 		int k1 = int32Key[1];
 		int k2 = int32Key[2];
 		int k3 = int32Key[3];
+		
+		// For each block of 8 bytes
 		for (int end = off + len; off < end; off += 8) {
+			
+			// Pack bytes into int32s in big endian
 			int y = (b[off + 0] & 0xFF) << 24
 			      | (b[off + 1] & 0xFF) << 16
 			      | (b[off + 2] & 0xFF) <<  8
@@ -44,10 +52,14 @@ final class TeaCipherer extends Cipherer {
 			      | (b[off + 5] & 0xFF) << 16
 			      | (b[off + 6] & 0xFF) <<  8
 			      | (b[off + 7] & 0xFF) <<  0;
+			
+			// The 32 rounds
 			for (int i = 0, sum = 0x9E3779B9; i < 32; i++, sum += 0x9E3779B9) {
 				y += ((z << 4) + k0) ^ (z + sum) ^ ((z >>> 5) + k1);
 				z += ((y << 4) + k2) ^ (y + sum) ^ ((y >>> 5) + k3);
 			}
+			
+			// Unpack int32s into bytes in big endian
 			b[off + 0] = (byte)(y >>> 24);
 			b[off + 1] = (byte)(y >>> 16);
 			b[off + 2] = (byte)(y >>> 8);
@@ -64,14 +76,18 @@ final class TeaCipherer extends Cipherer {
 		if (cipher == null)
 			throw new IllegalStateException("Already zeroized");
 		BoundsChecker.check(b.length, off, len);
-		if ((len & 7) != 0)
+		if (len % 8 != 0)
 			throw new IllegalArgumentException("Invalid block length");
 		
 		int k0 = int32Key[0];
 		int k1 = int32Key[1];
 		int k2 = int32Key[2];
 		int k3 = int32Key[3];
+		
+		// For each block of 8 bytes
 		for (int end = off + len; off < end; off += 8) {
+			
+			// Pack bytes into int32s in big endian
 			int y = (b[off + 0] & 0xFF) << 24
 			      | (b[off + 1] & 0xFF) << 16
 			      | (b[off + 2] & 0xFF) <<  8
@@ -80,10 +96,14 @@ final class TeaCipherer extends Cipherer {
 			      | (b[off + 5] & 0xFF) << 16
 			      | (b[off + 6] & 0xFF) <<  8
 			      | (b[off + 7] & 0xFF) <<  0;
+			
+			// The 32 rounds
 			for (int i = 0, sum = 0xC6EF3720; i < 32; i++, sum -= 0x9E3779B9) {
 				z -= ((y << 4) + k2) ^ (y + sum) ^ ((y >>> 5) + k3);
 				y -= ((z << 4) + k0) ^ (z + sum) ^ ((z >>> 5) + k1);
 			}
+			
+			// Unpack int32s into bytes in big endian
 			b[off + 0] = (byte)(y >>> 24);
 			b[off + 1] = (byte)(y >>> 16);
 			b[off + 2] = (byte)(y >>>  8);
--- Source/p79068/crypto/cipher/XteaCipherer.java
+++ Source/p79068/crypto/cipher/XteaCipherer.java
@@ -18,8 +18,12 @@ final class XteaCipherer extends Cipherer {
 	XteaCipherer(Xtea cipher, byte[] key) {
 		super(cipher, key);
 		int32Key = new int[4];
-		for (int i = 0; i < int32Key.length; i++)
-			int32Key[i] = key[i * 4] << 24 | (key[i * 4 + 1] & 0xFF) << 16 | (key[i * 4 + 2] & 0xFF) << 8 | (key[i * 4 + 3] & 0xFF);
+		for (int i = 0; i < int32Key.length; i++) {
+			int32Key[i] = (key[i * 4 + 0] & 0xFF) << 24
+			            | (key[i * 4 + 1] & 0xFF) << 16
+			            | (key[i * 4 + 2] & 0xFF) <<  8
+			            | (key[i * 4 + 3] & 0xFF) <<  0;
+		}
 	}
 	
 	
@@ -28,10 +32,13 @@ final class XteaCipherer extends Cipherer {
 		if (cipher == null)
 			throw new IllegalStateException("Already zeroized");
 		BoundsChecker.check(b.length, off, len);
-		if ((len & 7) != 0)
+		if (len % 8 != 0)
 			throw new IllegalArgumentException("Invalid block length");
 		
+		// For each block of 8 bytes
 		for (int end = off + len; off < end; off += 8) {
+			
+			// Pack bytes into int32s in big endian
 			int y = (b[off + 0] & 0xFF) << 24
 			      | (b[off + 1] & 0xFF) << 16
 			      | (b[off + 2] & 0xFF) <<  8
@@ -40,11 +47,15 @@ final class XteaCipherer extends Cipherer {
 			      | (b[off + 5] & 0xFF) << 16
 			      | (b[off + 6] & 0xFF) <<  8
 			      | (b[off + 7] & 0xFF) <<  0;
+			
+			// The 32 rounds
 			for (int i = 0, sum = 0; i < 32; i++) {
 				y += (((z << 4) ^ (z >>> 5)) + z) ^ (sum + int32Key[sum & 3]);
 				sum += 0x9E3779B9;
 				z += (((y << 4) ^ (y >>> 5)) + y) ^ (sum + int32Key[(sum >>> 11) & 3]);
 			}
+			
+			// Unpack int32s into bytes in big endian
 			b[off + 0] = (byte)(y >>> 24);
 			b[off + 1] = (byte)(y >>> 16);
 			b[off + 2] = (byte)(y >>>  8);
@@ -61,10 +72,13 @@ final class XteaCipherer extends Cipherer {
 		if (cipher == null)
 			throw new IllegalStateException("Already zeroized");
 		BoundsChecker.check(b.length, off, len);
-		if ((len & 7) != 0)
+		if (len % 8 != 0)
 			throw new IllegalArgumentException("Invalid block length");
 		
+		// For each block of 8 bytes
 		for (int end = off + len; off < end; off += 8) {
+			
+			// Pack bytes into int32s in big endian
 			int y = (b[off + 0] & 0xFF) << 24
 			      | (b[off + 1] & 0xFF) << 16
 			      | (b[off + 2] & 0xFF) <<  8
@@ -73,11 +87,15 @@ final class XteaCipherer extends Cipherer {
 			      | (b[off + 5] & 0xFF) << 16
 			      | (b[off + 6] & 0xFF) <<  8
 			      | (b[off + 7] & 0xFF) <<  0;
+			
+			// The 32 rounds
 			for (int i = 0, sum = 0xC6EF3720; i < 32; i++) {
 				z -= (((y << 4) ^ (y >>> 5)) + y) ^ (sum + int32Key[(sum >>> 11) & 3]);
 				sum -= 0x9E3779B9;
 				y -= (((z << 4) ^ (z >>> 5)) + z) ^ (sum + int32Key[sum & 3]);
 			}
+			
+			// Unpack int32s into bytes in big endian
 			b[off + 0] = (byte)(y >>> 24);
 			b[off + 1] = (byte)(y >>> 16);
 			b[off + 2] = (byte)(y >>>  8);
