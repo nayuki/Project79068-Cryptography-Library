07f79e6f4950318d0c65f7cffe43501684574537
Nayuki Minase <nayuki@eigenstate.org>
2008-05-18 20:45:54 +0000

    Reformatted code
    git-svn-id: file:///home/user/p79068-java-library-svn@360 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/math/RealMath.java
+++ Source/p79068/math/RealMath.java
@@ -7,7 +7,9 @@ Contains methods for math functions that deal with real numbers.
 */
 public class RealMath {
 	
-	/** Returns the sine of the specified number of complete revolutions. It is similar to <code>Math.sin(2*Math.PI*x)</code>. */
+	/**
+	 * Returns the sine of the specified number of complete revolutions. It is similar to <code>Math.sin(2*Math.PI*x)</code>.
+	 */
 	public static float sinRevs(float x) {
 		if (x >= 0f) {
 			if (x >= 1f)
@@ -22,7 +24,10 @@ public class RealMath {
 		return (float)((((((3.9653034141171493607e1) * y - 7.6564695440839330551e1) * y + 8.1601632709834543134e1) * y - 4.1341664669615459394e1) * y + 6.2831851989975942410e0) * x);
 	}
 	
-	/** Returns the cosine of the specified number of complete revolutions. It is similar to <code>Math.cos(2*Math.PI*x)</code>. */
+	
+	/**
+	 * Returns the cosine of the specified number of complete revolutions. It is similar to <code>Math.cos(2*Math.PI*x)</code>.
+	 */
 	public static float cosRevs(float x) {
 		float sign = 1;
 		if (x >= 0f) {
@@ -40,8 +45,12 @@ public class RealMath {
 		return (float)(sign * (((((5.6445734579155567248e1) * y - 8.5263204534087686108e1) * y + 6.4935380310507999466e1) * y - 1.9739179942871861928e1) * y + 9.9999996727011546714e-1));
 	}
 	
-	/** Returns the tangent of the specified number of complete revolutions. It is similar to <code>Math.tan(2*Math.PI*x)</code>. */
+	
+	/**
+	 * Returns the tangent of the specified number of complete revolutions. It is similar to <code>Math.tan(2*Math.PI*x)</code>.
+	 */
 	public static float tanRevs(float x) {
 		return sinRevs(x) / cosRevs(x);
 	}
+	
 }
