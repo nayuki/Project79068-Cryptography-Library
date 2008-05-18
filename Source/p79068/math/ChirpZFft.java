4b142df85475d888b731922618ada5f1cf17fc6e
Nayuki Minase <nayuki@eigenstate.org>
2008-05-18 20:44:28 +0000

    Cleaned up code; added error check
    git-svn-id: file:///home/user/p79068-java-library-svn@359 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/math/ChirpZFft.java
+++ Source/p79068/math/ChirpZFft.java
@@ -10,50 +10,64 @@ package p79068.math;
 final class ChirpZFft extends Dft {
 	
 	private int length;
-	private int convlen;  // The smallest power of 2 such that convlen >= len * 2 - 1 .
+	private int convolutionLength;  // The smallest power of 2 such that convlen >= len * 2 - 1 .
 	private Dft fft;
 	private double[] cos, sin;
 	private double[] convre, convim;
 	
 	
 	
-	ChirpZFft(int len) {
-		if (len < 1)
+	ChirpZFft(int length) {
+		if (length < 1)
 			throw new IllegalArgumentException("Length must be positive");
-		length = len;
-		for (convlen = 1; convlen < length * 2 - 1; convlen *= 2);
+		if (length > (1 << 29))
+			throw new IllegalArgumentException("Length too large");
+		
+		this.length = length;
+		for (convolutionLength = 1; convolutionLength < length * 2 - 1; convolutionLength *= 2);
+		
 		cos = new double[length];
 		sin = new double[length];
-		convre = new double[convlen];
-		convim = new double[convlen];
-		cos[0] = 1;
-		sin[0] = 0;
-		convre[0] = cos[0] / convlen;
-		convim[0] = sin[0] / convlen;
-		for (int i = 1; i < length; i++) {
-			convre[i] = convre[convlen - i] = (cos[i] = Math.cos((long)i * i % (length * 2) * Math.PI / length)) / convlen;
-			convim[i] = convim[convlen - i] = (sin[i] = Math.sin((long)i * i % (length * 2) * Math.PI / length)) / convlen;
+		for (int i = 0; i < length; i++) {
+			cos[i] = Math.cos((long)i * i % (length * 2) * Math.PI / length);
+			sin[i] = Math.sin((long)i * i % (length * 2) * Math.PI / length);
 		}
-		fft = new Fft(convlen);
+		
+		convre = new double[convolutionLength];
+		convim = new double[convolutionLength];
+		convre[0] = cos[0] / convolutionLength;
+		convim[0] = sin[0] / convolutionLength;
+		for (int i = 1; i < length; i++) {
+			convre[i] = cos[i] / convolutionLength;
+			convim[i] = convim[convolutionLength - i] = sin[i] / convolutionLength;
+			convre[convolutionLength - i] = convre[i];
+			convim[convolutionLength - i] = convim[i];
+		}
+		
+		fft = new Fft(convolutionLength);
 		fft.transform(convre, convim);
 	}
 	
 	
 	
 	public void transform(double[] inre, double[] inim, double[] outre, double[] outim) {
-		double[] tpre = new double[convlen];
-		double[] tpim = new double[convlen];
+		double[] tpre = new double[convolutionLength];
+		double[] tpim = new double[convolutionLength];
 		for (int i = 0; i < length; i++) {
 			tpre[i] = inre[i] * cos[i] + inim[i] * sin[i];
 			tpim[i] = -inre[i] * sin[i] + inim[i] * cos[i];
 		}
+		
 		fft.transform(tpre, tpim);
-		for (int i = 0; i < convlen; i++) {
+		
+		for (int i = 0; i < convolutionLength; i++) {
 			double re = tpre[i] * convre[i] - tpim[i] * convim[i];
 			tpim[i] = tpre[i] * convim[i] + tpim[i] * convre[i];
 			tpre[i] = re;
 		}
+		
 		fft.transformInverse(tpre, tpim);
+		
 		for (int i = 0; i < length; i++) {
 			outre[i] = tpre[i] * cos[i] + tpim[i] * sin[i];
 			outim[i] = -tpre[i] * sin[i] + tpim[i] * cos[i];
