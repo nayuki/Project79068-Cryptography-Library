657086879ec481a072fda12fe31437eedd5640c4
Nayuki Minase <nayuki@eigenstate.org>
2008-05-18 21:02:40 +0000

    Updated algorithms
    git-svn-id: file:///home/user/p79068-java-library-svn@361 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/math/RealMath.java
+++ Source/p79068/math/RealMath.java
@@ -19,6 +19,12 @@ public class RealMath {
 			else if (x >= 0.25f)
 				x = 0.5f - x;
 		} else {
+			if (x <= -1f)
+				x %= 1;
+			if (x <= -0.75f)
+				x++;
+			else if (x <= -0.25f)
+				x = -0.5f - x;
 		}
 		double y = x * x;
 		return (float)((((((3.9653034141171493607e1) * y - 7.6564695440839330551e1) * y + 8.1601632709834543134e1) * y - 4.1341664669615459394e1) * y + 6.2831851989975942410e0) * x);
@@ -30,16 +36,15 @@ public class RealMath {
 	 */
 	public static float cosRevs(float x) {
 		float sign = 1;
-		if (x >= 0f) {
-			if (x >= 1f)
-				x %= 1;
-			if (x >= 0.75f)
-				x--;
-			else if (x >= 0.25f) {
-				x = 0.5f - x;
-				sign = -1;
-			}
-		} else {
+		if (x < 0f)
+			x = -x;
+		if (x >= 1f)
+			x %= 1;
+		if (x >= 0.75f)
+			x--;
+		else if (x >= 0.25f) {
+			x = 0.5f - x;
+			sign = -1;
 		}
 		double y = x * x;
 		return (float)(sign * (((((5.6445734579155567248e1) * y - 8.5263204534087686108e1) * y + 6.4935380310507999466e1) * y - 1.9739179942871861928e1) * y + 9.9999996727011546714e-1));
@@ -52,5 +57,12 @@ public class RealMath {
 	public static float tanRevs(float x) {
 		return sinRevs(x) / cosRevs(x);
 	}
+	
+	
+	
+	/**
+	 * Not instantiable.
+	 */
+	private RealMath() {}
 	
 }
