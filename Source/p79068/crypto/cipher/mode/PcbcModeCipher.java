afad92b1ed647a0d77c60512d060a742f8a9be79
Nayuki Minase <nayuki@eigenstate.org>
2008-05-31 16:01:54 +0000

    Reverted changeset 376
    git-svn-id: file:///home/user/p79068-java-library-svn@377 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/crypto/cipher/mode/PcbcModeCipher.java
+++ Source/p79068/crypto/cipher/mode/PcbcModeCipher.java
@@ -9,13 +9,13 @@ import p79068.crypto.cipher.Cipherer;
 /**
  * A cipher using a block cipher in PCBC (propagating cipher block chaining) mode.
  * <p>Encryption algorithm:</p>
- * <p><code>plain XOR cipher[-1] = initialization vector<br>
- * plain XOR cipher[i] = plaintext[i] XOR ciphertext[i]<br>
- * ciphertext[i] = encrypt(plaintext[i] XOR plain XOR cipher[i-1])</code></p>
+ * <p><code>plain xor cipher[-1] = initialization vector<br>
+ * plain xor cipher[i] = plaintext[i] xor ciphertext[i]<br>
+ * ciphertext[i] = encrypt(plaintext[i] XOR plain xor cipher[i-1])</code></p>
  * <p>Decryption algorithm:</p>
- * <p><code>plain XOR cipher[-1] = initialization vector<br>
- * plain XOR cipher[i] = plaintext[i] XOR ciphertext[i]<br>
- * plaintext[i] = decrypt(ciphertext[i]) XOR plain XOR cipher[i-1]</code></p>
+ * <p><code>plain xor cipher[-1] = initialization vector<br>
+ * plain xor cipher[i] = plaintext[i] xor ciphertext[i]<br>
+ * plaintext[i] = decrypt(ciphertext[i]) XOR plain xor cipher[i-1]</code></p>
  */
 public final class PcbcModeCipher extends Cipher implements Zeroizable {
 	
