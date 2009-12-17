fb5b9b3ee99f0ce03f384be736ad1988fd06d687
Nayuki Minase <nayuki@eigenstate.org>
2009-12-17 09:27:38 +0000

    Added negative number test cases.
    git-svn-id: file:///home/user/p79068-java-library-svn@413 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Tests/math/KaratsubaMultiplicationTest.java
+++ Tests/math/KaratsubaMultiplicationTest.java
@@ -13,9 +13,33 @@ public class KaratsubaMultiplicationTest {
 	
 	
 	@Test
-	public void testBig() {
+	public void testBig0() {
 		BigInteger x = new BigInteger(30000, random);
 		BigInteger y = new BigInteger(30000, random);
+		assertEquals(x.multiply(y), KaratsubaMultiplication.multiply(x, y));
+	}
+	
+	
+	@Test
+	public void testBig1() {
+		BigInteger x = new BigInteger(30000, random);
+		BigInteger y = new BigInteger(30000, random).negate();
+		assertEquals(x.multiply(y), KaratsubaMultiplication.multiply(x, y));
+	}
+	
+	
+	@Test
+	public void testBig2() {
+		BigInteger x = new BigInteger(30000, random).negate();
+		BigInteger y = new BigInteger(30000, random);
+		assertEquals(x.multiply(y), KaratsubaMultiplication.multiply(x, y));
+	}
+	
+	
+	@Test
+	public void testBig3() {
+		BigInteger x = new BigInteger(30000, random).negate();
+		BigInteger y = new BigInteger(30000, random).negate();
 		assertEquals(x.multiply(y), KaratsubaMultiplication.multiply(x, y));
 	}
 	
