7830c1a165134c0df55a02ea2ba7d441b4142fd9
Nayuki Minase <nayuki@eigenstate.org>
2009-12-17 20:25:18 +0000

    Null checker test: Updated code.
    git-svn-id: file:///home/user/p79068-java-library-svn@438 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Tests/p79068/lang/NullCheckerTest.java
+++ Tests/p79068/lang/NullCheckerTest.java
@@ -20,7 +20,23 @@ public class NullCheckerTest {
 	
 	@Test(expected = NullPointerException.class)
 	public void testCheckNull() {
-		NullChecker.check(null);
+		NullChecker.check((Object)null);
+	}
+	
+	
+	@Test
+	public void testCheckVarargNotNull() {
+		try {
+			NullChecker.check(new Object(), new String("a"), new Integer(3));
+		} catch (NullPointerException e) {
+			fail();
+		}
+	}
+	
+	
+	@Test(expected = NullPointerException.class)
+	public void testCheckVarargNull() {
+		NullChecker.check(new Object(), null);
 	}
 	
 }
