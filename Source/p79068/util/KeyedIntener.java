24d8bf2738fa014dc065cc5278a4d7172e5ff9a3
Nayuki Minase <nayuki@eigenstate.org>
2009-12-17 20:12:05 +0000

    Keyed interner: Fixed code.
    git-svn-id: file:///home/user/p79068-java-library-svn@435 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/util/KeyedIntener.java
+++ Source/p79068/util/KeyedIntener.java
@@ -6,6 +6,7 @@ import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
 import p79068.lang.NullChecker;
+import p79068.math.Fft;
 
 
 public final class KeyedIntener<K,V> {
@@ -24,7 +25,7 @@ public final class KeyedIntener<K,V> {
 		queue = new ReferenceQueue<V>();
 	}
 	
-	
+	boolean x;
 	
 	public synchronized V get(K key) {
 		NullChecker.check(key);
@@ -37,26 +38,41 @@ public final class KeyedIntener<K,V> {
 	
 	
 	public synchronized void put(K key, V value) {
+		cleanup();
 		NullChecker.check(key);
 		NullChecker.check(value);
-		if (!cache.containsKey(key) || cache.get(key).get() == null) {
+		boolean put = false;
+		if (!cache.containsKey(key))
+			put = true;
+		else {
+			WeakReference<V> oldref = cache.get(key);
+			if (oldref.get() == null) {
+				inverseMap.remove(oldref);
+				put = true;
+			}
+		}
+		if (put) {
 			WeakReference<V> ref = new WeakReference<V>(value, queue);
 			cache.put(key, ref);
 			inverseMap.put(ref, key);
+			assert cache.size() == inverseMap.size();
 		}
 	}
-	
+
 	
 	private void cleanup() {
-		Reference<? extends V> ref = queue.poll();
-		if (ref != null) {
+		while (true) {
+			Reference<? extends V> ref = queue.poll();
+			if (ref == null)
+				break;
 			if (!inverseMap.containsKey(ref))
-				throw new AssertionError();
+				continue;
 			K key = inverseMap.get(ref);
 			if (!cache.containsKey(key))
 				throw new AssertionError();
 			inverseMap.remove(ref);
 			cache.remove(key);
+			assert cache.size() == inverseMap.size();
 		}
 	}
 	
