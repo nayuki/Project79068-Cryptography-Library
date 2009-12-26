46e5db2c04a6f938cff6abffe997b3b14b82e1ba
Nayuki Minase <nayuki@eigenstate.org>
2009-12-26 17:44:09 +0000

    File I/O: Updated code
    git-svn-id: file:///home/user/p79068-java-library-svn@467 24ebd69f-44c7-534f-88b9-abd8d07edc6a

--- Source/p79068/io/FileIo.java
+++ Source/p79068/io/FileIo.java
@@ -1,6 +1,13 @@
 package p79068.io;
 
-import java.io.*;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 
 public class FileIo {
@@ -8,19 +15,23 @@ public class FileIo {
 	public static byte[] readAll(File file) throws IOException {
 		if (file.length() > Integer.MAX_VALUE)
 			throw new RuntimeException("File too large");
-		int len = (int)file.length();
-		byte[] b = new byte[len];
+		int length = (int)file.length();
+		byte[] b = new byte[length];
 		InputStream in = new FileInputStream(file);
-		int read = 0;
-		while (read < len) {
-			int tp = in.read(b, read, len - read);
-			if (tp == -1)
-				throw new RuntimeException("Unexpected end of file");
-			read += tp;
+		try {
+			int position = 0;
+			while (position < length) {
+				int read = in.read(b, position, length - position);
+				if (read == -1)
+					throw new EOFException("Unexpected end of file");
+				position += read;
+			}
+		} finally {
+			in.close();
 		}
-		in.close();
 		return b;
 	}
+	
 	
 	public static String readAll(File file, String charset) throws IOException {
 		InputStream in0 = new FileInputStream(file);
@@ -38,9 +49,14 @@ public class FileIo {
 		return sb.toString();
 	}
 	
+	
 	public static void write(File file, byte[] b) throws IOException {
 		OutputStream out = new FileOutputStream(file);
-		out.write(b);
-		out.close();
+		try {
+			out.write(b);
+		} finally {
+			out.close();
+		}
 	}
+	
 }
