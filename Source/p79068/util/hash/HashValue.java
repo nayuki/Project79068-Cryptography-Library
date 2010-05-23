a915f638f77a3d5791265418144e7cdaf6ad768b
Nayuki Minase <nayuki@eigenstate.org>
2010-05-23 02:05:23 +0000

    Hash value: Added and updated Javadoc comments, rearranged code.

--- Source/p79068/util/hash/HashValue.java
+++ Source/p79068/util/hash/HashValue.java
@@ -18,6 +18,11 @@ public final class HashValue implements Comparable<HashValue> {
 	
 	
 	
+	/**
+	 * Constructs a hash value from the specified byte array. The array's contents are cloned.
+	 * @param hash the byte array representing the hash
+	 * @throws NullPointerException if {@code hash} is {@code null}
+	 */
 	public HashValue(byte[] hash) {
 		NullChecker.check(hash);
 		hashValue = hash.clone();
@@ -34,7 +39,7 @@ public final class HashValue implements Comparable<HashValue> {
 	
 	
 	/**
-	 * Returns this hash value as a hexadecimal string (in uppercase). For example, <samp>"1337C0DE"</samp>.
+	 * Returns this hash value as a hexadecimal string (in uppercase). For example, {@code "1337C0DE"}.
 	 */
 	public String toHexString() {
 		StringBuilder sb = new StringBuilder();
@@ -53,15 +58,26 @@ public final class HashValue implements Comparable<HashValue> {
 	}
 	
 	
+	/**
+	 * Compares the specified object with this hash value for equality. Returns <code>true</code> if the specified object is a hash value and its byte array has the same length and contents as this hash value's byte array. Otherwise, this method returns <code>false</code>.
+	 * @param other the object to compare to
+	 * @return whether the <code>other</code> is a hash value with the same sequence of bytes
+	 */
 	@Override
-	public boolean equals(Object o) {
-		if (!(o instanceof HashValue))
+	public boolean equals(Object other) {
+		if (other instanceof HashValue)
+			return Arrays.equals(hashValue, ((HashValue)other).hashValue);
+		else
 			return false;
-		HashValue hash = (HashValue)o;
-		return Arrays.equals(hashValue, hash.hashValue);
 	}
 	
 	
+	/**
+	 * Compares this hash value with the specified hash value for order. Returns a negative integer, zero, or positive integer respectively if this hash value is less than, equal to, or greater than the specified hash value. The comparison involves lexicographical ordering on unsigned byte values. Hash values of different lengths are incomparable and an exception will be thrown.
+	 * @param other the object to compare to
+	 * @return a negative integer, zero, or positive integer respectively if {@code this} is less than, equal to, or greater than {@code other}
+	 * @throws IllegalArgumentException if {@code other} has a different hash length than {@code this}
+	 */
 	public int compareTo(HashValue other) {
 		NullChecker.check(other);
 		if (hashValue.length != other.hashValue.length)
@@ -76,12 +92,12 @@ public final class HashValue implements Comparable<HashValue> {
 	
 	
 	/**
-	 * Returns the hash code for this hash value.
+	 * Returns the hash code for this hash value. The hash code algorithm is subjected to change.
 	 * @return the hash code for this hash value
 	 */
 	@Override
 	public int hashCode() {
-		// Directly uses the leading 4 (or fewer) bytes as the result
+		// Directly uses the leading 4 (or fewer) bytes as the result, because we assume that the hash function that produced this hash value is good
 		int result = 0;
 		for (int i = 0; i < Math.min(hashValue.length, 4); i++)
 			result = result << 8 | (hashValue[i] & 0xFF);
@@ -90,7 +106,7 @@ public final class HashValue implements Comparable<HashValue> {
 	
 	
 	/**
-	 * Returns a string representation of this hash value. Currently, the hexadecimal hash value is returned. This is subjected to change.
+	 * Returns a string representation of this hash value. Currently, the hexadecimal hash value is returned. The string's format is subjected to change.
 	 * @return a string representation of this hash value
 	 */
 	@Override
