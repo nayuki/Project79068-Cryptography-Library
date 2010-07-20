818ae503e5e561a44366adc2c1b09d26bf8b1320
Nayuki Minase <nayuki@eigenstate.org>
2010-07-20 03:58:56 +0000

    File utils: Changed returns from list to sorted set.

--- Source/p79068/io/FileUtils.java
+++ Source/p79068/io/FileUtils.java
@@ -2,35 +2,37 @@ package p79068.io;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 
 public final class FileUtils {
 	
 	/**
-	 * Returns a list of all items in the specified directory.
-	 * @return a list of all items in {@code dir}
+	 * Returns a sorted set of all items in the specified directory.
+	 * @return a sorted set of all items in {@code dir}
 	 * @throws IllegalArgumentException if {@code dir} is not a directory
 	 */
-	public static List<File> listItems(File dir) {
+	public static SortedSet<File> listItems(File dir) {
 		if (!dir.isDirectory())
 				throw new IllegalArgumentException("Not a directory");
-		return Arrays.asList(dir.listFiles());
+		SortedSet<File> result = new TreeSet<File>();
+		Collections.addAll(result, dir.listFiles());
+		return result;
 	}
 	
 	
 	/**
-	 * Returns a list of all files in the specified directory.
-	 * @return a list of all files in {@code dir}
+	 * Returns a sorted set of all files in the specified directory.
+	 * @return a sorted set of all files in {@code dir}
 	 * @throws IllegalArgumentException if {@code dir} is not a directory
 	 */
-	public static List<File> listFiles(File dir) {
+	public static SortedSet<File> listFiles(File dir) {
 		if (!dir.isDirectory())
 			throw new IllegalArgumentException("Not a directory");
 		
-		List<File> result = new ArrayList<File>();
+		SortedSet<File> result = new TreeSet<File>();
 		for (File item : dir.listFiles()) {
 			if (item.isFile())
 				result.add(item);
@@ -40,15 +42,15 @@ public final class FileUtils {
 	
 	
 	/**
-	 * Returns a list of all directories in the specified directory.
-	 * @return a list of all directories in {@code dir}
+	 * Returns a sorted set of all directories in the specified directory.
+	 * @return a sorted set of all directories in {@code dir}
 	 * @throws IllegalArgumentException if {@code dir} is not a directory
 	 */
-	public static List<File> listDirs(File dir) {
+	public static SortedSet<File> listDirs(File dir) {
 		if (!dir.isDirectory())
 			throw new IllegalArgumentException("Not a directory");
 		
-		List<File> result = new ArrayList<File>();
+		SortedSet<File> result = new TreeSet<File>();
 		for (File item : dir.listFiles()) {
 			if (item.isDirectory())
 				result.add(item);
