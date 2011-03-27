179bc0ca79781d70f7ed3f075fe655dd6e941869
Nayuki Minase <nayuki@eigenstate.org>
2011-03-27 02:52:37 +0000

    StreamCiphererRandom: Tweaked code.

--- Source/p79068/crypto/cipher/StreamCiphererRandom.java
+++ Source/p79068/crypto/cipher/StreamCiphererRandom.java
@@ -1,5 +1,7 @@
 package p79068.crypto.cipher;
 
+import java.util.Arrays;
+
 import p79068.random.AbstractRandom;
 
 
@@ -21,7 +23,10 @@ public final class StreamCiphererRandom extends AbstractRandom {
 	public int uniformInt() {
 		byte[] b = new byte[4];
 		cipherer.encrypt(b, 0, b.length);
-		return b[0] << 24 | (b[1] & 0xFF) << 16 | (b[2] & 0xFF) << 8 | (b[3] & 0xFF);
+		return b[0] << 24
+		    | (b[1] & 0xFF) << 16
+		    | (b[2] & 0xFF) <<  8
+		    | (b[3] & 0xFF);
 	}
 	
 	
@@ -29,20 +34,20 @@ public final class StreamCiphererRandom extends AbstractRandom {
 	public long uniformLong() {
 		byte[] b = new byte[8];
 		cipherer.encrypt(b, 0, b.length);
-		return (long)b[0] << 56 | (b[1] & 0xFFL) << 48 | (b[2] & 0xFFL) << 40 | (b[3] & 0xFFL) << 32 | (b[4] & 0xFFL) << 24 | (b[5] & 0xFFL) << 16 | (b[6] & 0xFFL) << 8 | (b[7] & 0xFFL);
-	}
-	
-	
-	@Override
-	public double uniformDouble() {
-		return (uniformLong() & 0x1FFFFFFFFFFFFFL) * doubleScaler;
+		return (long)b[0] << 56
+		    | (b[1] & 0xFFL) << 48
+		    | (b[2] & 0xFFL) << 40
+		    | (b[3] & 0xFFL) << 32
+		    | (b[4] & 0xFFL) << 24
+		    | (b[5] & 0xFFL) << 16
+		    | (b[6] & 0xFFL) <<  8
+		    | (b[7] & 0xFFL);
 	}
 	
 	
 	@Override
 	public void uniformBytes(byte[] b, int off, int len) {
-		for (int i = off, end = off + len; i < end; i++)
-			b[i] = 0;  // This is not strictly necessary.
+		Arrays.fill(b, off, off + len, (byte)0);  // This is not strictly necessary
 		cipherer.encrypt(b, off, len);
 	}
 	
