ad1e51e8c6a0d4ce763b52df1419b18971b738be
Nayuki Minase <nayuki@eigenstate.org>
2011-07-25 03:28:29 +0000

    Merged class AbstractSha1Core into Sha1Core.

--- src/p79068/crypto/hash/Sha1Core.java
+++ src/p79068/crypto/hash/Sha1Core.java
@@ -1,20 +1,46 @@
 package p79068.crypto.hash;
 
 import static p79068.math.IntegerBitMath.rotateLeft;
+
+import java.util.Arrays;
+
+import p79068.crypto.Zeroizer;
+import p79068.hash.HashValue;
 import p79068.lang.BoundsChecker;
+import p79068.math.IntegerBitMath;
 
 
-final class Sha1Core extends AbstractSha1Core {
+class Sha1Core extends BlockHasherCore {
+	
+	private int[] state;
 	
 	private final boolean sha1Mode;
 	
 	
 	
 	public Sha1Core(boolean sha1Mode) {
-		super();
+		state = new int[]{0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0};
 		this.sha1Mode = sha1Mode;
 	}
 	
+	
+	
+	@Override
+	public Sha1Core clone() {
+		if (state == null)
+			throw new IllegalStateException("Already zeroized");
+		Sha1Core result = (Sha1Core)super.clone();
+		result.state = result.state.clone();
+		return result;
+	}
+	
+	
+	@Override
+	public void zeroize() {
+		if (state == null)
+			throw new IllegalStateException("Already zeroized");
+		state = Zeroizer.clear(state);
+	}
 	
 	
 	@Override
@@ -57,9 +83,7 @@ final class Sha1Core extends AbstractSha1Core {
 	}
 	
 	
-	
 	private static final int[] k = {0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xCA62C1D6};
-	
 	
 	/*
 	 * Each round performs a transform of this form:
@@ -139,6 +163,22 @@ final class Sha1Core extends AbstractSha1Core {
 		else if (40 <= i && i < 60) return (x & y) ^ (x & z) ^ (y & z);  // Majority. Can be optimized to (x & (y | z)) | (y & z).
 		else if (60 <= i && i < 80) return x ^ y ^ z;                    // Parity
 		else throw new AssertionError();
+	}
+	
+	
+	@Override
+	public HashValue getHashDestructively(byte[] block, int blockLength, long length) {
+		block[blockLength] = (byte)0x80;
+		blockLength++;
+		Arrays.fill(block, blockLength, block.length, (byte)0);
+		if (blockLength + 8 > block.length) {
+			compress(block);
+			Arrays.fill(block, (byte)0);
+		}
+		for (int i = 0; i < 8; i++)
+			block[block.length - 1 - i] = (byte)((length * 8) >>> (i * 8));
+		compress(block);
+		return new HashValue(IntegerBitMath.toBytesBigEndian(state));
 	}
 	
 }
