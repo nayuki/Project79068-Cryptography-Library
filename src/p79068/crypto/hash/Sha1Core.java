nayuki/Project79068-Cryptography-Library

commit 5aeb7fb4b36401d9526a62511262c3a1d3d9862e

Removed support for the original SHA algorithm ("SHA-0").

src/p79068/crypto/hash/Sha1Core.java
@@ -6,13 +6,8 @@ import p79068.lang.BoundsChecker;
 
 final class Sha1Core extends AbstractSha1Core {
 	
-	private final boolean sha1Mode;
-	
-	
-	
-	public Sha1Core(boolean sha1Mode) {
+	public Sha1Core() {
 		super();
-		this.sha1Mode = sha1Mode;
 	}
 	
 	
@@ -40,8 +35,6 @@ final class Sha1Core extends AbstractSha1Core {
 			// Expand the schedule
 			for (int i = 16; i < 80; i++) {
 				int temp = schedule[i - 3] ^ schedule[i - 8] ^ schedule[i - 14] ^ schedule[i - 16];
-				if (sha1Mode)  // This is the only difference between SHA and SHA-1
-					temp = rotateLeft(temp, 1);
 				schedule[i] = temp;
 			}
 			

src/p79068/crypto/hash/Sha.java
@@ -9,12 +9,6 @@ import p79068.hash.Hasher;
 public final class Sha extends AbstractBlockHashFunction {
 	
 	/**
-	 * The original SHA hash function, also known as SHA-0. {@code name = "SHA"}, {@code hashLength = 20}, {@code blockLength = 64}. Described in FIPS Publication 180, 1993-05-11.
-	 */
-	public final static Sha SHA_FUNCTION = new Sha("SHA", 20, 64);
-	
-	
-	/**
 	 * The SHA-1 hash function. {@code name = "SHA-1"}, {@code hashLength = 20}, {@code blockLength = 64}.
 	 */
 	public final static Sha SHA1_FUNCTION = new Sha("SHA-1", 20, 64);
@@ -57,8 +51,7 @@ public final class Sha extends AbstractBlockHashFunction {
 	 */
 	@Override
 	public Hasher newHasher() {
-		if      (this == SHA_FUNCTION   ) return new BlockHasher(this, new Sha1Core(false));
-		else if (this == SHA1_FUNCTION  ) return new BlockHasher(this, new FastSha1Core());
+		if      (this == SHA1_FUNCTION  ) return new BlockHasher(this, new FastSha1Core());
 		else if (this == SHA224_FUNCTION) return new BlockHasher(this, new Sha256Core(false));
 		else if (this == SHA256_FUNCTION) return new BlockHasher(this, new Sha256Core(true));
 		else if (this == SHA384_FUNCTION) return new BlockHasher(this, new Sha512Core(false));

test/p79068/crypto/hash/ShaTest.java
@@ -19,18 +19,6 @@ public final class ShaTest {
 	
 	
 	@Test
-	public void testSha() {
-		HashUtils.testAscii(Sha.SHA_FUNCTION, msg0, "F96CEA198AD1DD5617AC084A3D92C6107708C0EF");
-		HashUtils.testAscii(Sha.SHA_FUNCTION, msg1, "37F297772FAE4CB1BA39B6CF9CF0381180BD62F2");
-		HashUtils.testAscii(Sha.SHA_FUNCTION, msg2, "0164B8A914CD2A5E74C4F7FF082C4D97F1EDF880");
-		HashUtils.testAscii(Sha.SHA_FUNCTION, msg3, "C1B0F222D150EBB9AA36A40CAFDC8BCBED830B14");
-		HashUtils.testAscii(Sha.SHA_FUNCTION, msg4, "B40CE07A430CFD3C033039B9FE9AFEC95DC1BDCD");
-		HashUtils.testAscii(Sha.SHA_FUNCTION, msg5, "D2516EE1ACFA5BAF33DFC1C471E438449EF134C8");
-		HashUtils.testAscii(Sha.SHA_FUNCTION, msg7, "3232AFFA48628A26653B5AAA44541FD90D690603");
-	}
-	
-	
-	@Test
 	public void testSha1() {
 		HashUtils.testAscii(Sha.SHA1_FUNCTION, msg0, "DA39A3EE5E6B4B0D3255BFEF95601890AFD80709");
 		HashUtils.testAscii(Sha.SHA1_FUNCTION, msg1, "86F7E437FAA5A7FCE15D1DDCB9EAEAEA377667B8");
@@ -88,7 +76,6 @@ public final class ShaTest {
 	
 	@Test
 	public void testZeroize() {
-		HashUtils.testZeroization(Sha.SHA_FUNCTION);
 		HashUtils.testZeroization(Sha.SHA1_FUNCTION);
 		HashUtils.testZeroization(Sha.SHA224_FUNCTION);
 		HashUtils.testZeroization(Sha.SHA256_FUNCTION);

commit 81da4762428efeced0f434a434a723f14a9ff6ef

Sha1Core: Fixed the algorithm. Without this fix, the algorithm is actually SHA-0. The mistake was introduced by commit 5aeb7fb4b36401d9526a62511262c3a1d3d9862e.

src/p79068/crypto/hash/Sha1Core.java
@@ -35,6 +35,7 @@ final class Sha1Core extends AbstractSha1Core {
 			// Expand the schedule
 			for (int i = 16; i < 80; i++) {
 				int temp = schedule[i - 3] ^ schedule[i - 8] ^ schedule[i - 14] ^ schedule[i - 16];
+				temp = rotateLeft(temp, 1);
 				schedule[i] = temp;
 			}
 			