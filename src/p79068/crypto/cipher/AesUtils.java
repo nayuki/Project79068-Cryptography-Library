7f87022a97a045a4ec509073575fff5e3bbe0085
Nayuki Minase <nayuki@eigenstate.org>
2011-07-25 03:33:34 +0000

    AesUtils: Tweaked code without changing functionality.

--- src/p79068/crypto/cipher/AesUtils.java
+++ src/p79068/crypto/cipher/AesUtils.java
@@ -12,21 +12,8 @@ final class AesUtils {
 	
 	
 	static {
+		// Initialize exponentiation table
 		exp = new int[255];
-		log = new int[256];
-		initExpLogTables();
-		
-		SBOX = new byte[256];
-		SBOX_INVERSE = new byte[256];
-		initSboxes();
-	}
-	
-	
-	/**
-	 * Initializes the exponentiation and logarithm tables.
-	 */
-	private static void initExpLogTables() {
-		// Initialize exponentiation (exp) table
 		int product = 1;
 		for (int i = 0; i < exp.length; i++) {
 			exp[i] = product;
@@ -36,40 +23,20 @@ final class AesUtils {
 		}
 		
 		// Initialize logarithm (log) table
+		log = new int[256];
 		log[0] = Integer.MIN_VALUE;  // Log of 0 is invalid
 		for (int i = 1; i < exp.length; i++)
 			log[exp[i]] = i;
-	}
-	
-	
-	private static void initSboxes() {
+		
+		// Initialize the S-box and inverse
+		SBOX = new byte[256];
+		SBOX_INVERSE = new byte[256];
 		for (int i = 0; i < 256; i++) {
-			int temp = reciprocal(i);
-			int s = ((temp ^ (temp << 4 | temp >>> 4) ^ (temp << 3 | temp >>> 5) ^ (temp << 2 | temp >>> 6) ^ (temp << 1 | temp >>> 7) ^ 0x63)) & 0xFF;
+			int tp = reciprocal(i);
+			int s = ((tp ^ (tp << 4 | tp >>> 4) ^ (tp << 3 | tp >>> 5) ^ (tp << 2 | tp >>> 6) ^ (tp << 1 | tp >>> 7) ^ 0x63)) & 0xFF;
 			SBOX[i] = (byte)s;
 			SBOX_INVERSE[s] = (byte)i;
 		}
-	}
-	
-	
-	
-	public static int multiply(int x, int y) {
-		if ((x & 0xFF) != x || (y & 0xFF) != y)
-			throw new IllegalArgumentException("Input out of range");
-		else if (x == 0 || y == 0)
-			return 0;
-		else
-			return exp[(log[x] + log[y]) % 255];
-	}
-	
-	
-	public static int reciprocal(int x) {
-		if ((x & 0xFF) != x)
-			throw new IllegalArgumentException("Input out of range");
-		else if (x != 0)
-			return exp[(255 - log[x]) % 255];
-		else
-			return 0;
 	}
 	
 	
@@ -80,6 +47,26 @@ final class AesUtils {
 	
 	public static byte[] getSboxInverse() {
 		return SBOX_INVERSE.clone();
+	}
+	
+	
+	public static int multiply(int x, int y) {
+		if ((x & 0xFF) != x || (y & 0xFF) != y)
+			throw new IllegalArgumentException("Input out of range");
+		if (x == 0 || y == 0)
+			return 0;
+		else
+			return exp[(log[x] + log[y]) % 255];
+	}
+	
+	
+	public static int reciprocal(int x) {
+		if ((x & 0xFF) != x)
+			throw new IllegalArgumentException("Input out of range");
+		if (x == 0)
+			return 0;
+		else
+			return exp[(255 - log[x]) % 255];
 	}
 	
 	
@@ -103,7 +90,8 @@ final class AesUtils {
 	}
 	
 	
-	private static int subInt32Bytes(int x) {  // Apply S-box to each byte in the 32-bit integer.
+	// Applies the S-box to each byte in the 32-bit integer.
+	private static int subInt32Bytes(int x) {
 		return (SBOX[x >>> 24 & 0xFF] & 0xFF) << 24
 		     | (SBOX[x >>> 16 & 0xFF] & 0xFF) << 16
 		     | (SBOX[x >>>  8 & 0xFF] & 0xFF) <<  8
@@ -111,11 +99,12 @@ final class AesUtils {
 	}
 	
 	
+	// Converts 4 consecutive bytes into an int32 in big endian.
 	private static int toInt32(byte[] b, int off) {
-		return (b[off + 0] & 0xFF) << 24
-		     | (b[off + 1] & 0xFF) << 16
-		     | (b[off + 2] & 0xFF) <<  8
-		     | (b[off + 3] & 0xFF) <<  0;
+		return   (b[off + 0] & 0xFF) << 24
+		       | (b[off + 1] & 0xFF) << 16
+		       | (b[off + 2] & 0xFF) <<  8
+		       | (b[off + 3] & 0xFF) <<  0;
 	}
 	
 	
