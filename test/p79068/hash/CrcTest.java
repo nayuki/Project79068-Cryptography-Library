e7cee5059ae9857b507505b64ee9d8ea77a4a9e9
Nayuki Minase <nayuki@eigenstate.org>
2013-02-27 15:34:29 +0000

    CrcTest: Added one-bit error test cases.

--- test/p79068/hash/CrcTest.java
+++ test/p79068/hash/CrcTest.java
@@ -1,11 +1,18 @@
 package p79068.hash;
 
-import org.junit.Test;
+import static org.junit.Assert.assertFalse;
 import static p79068.hash.HashUtils.testAscii;
 import static p79068.hash.HashUtils.testHex;
 
+import org.junit.Test;
+
+import p79068.util.random.Random;
+
 
 public final class CrcTest {
+	
+	private static Random random = Random.DEFAULT;
+	
 	
 	@Test
 	public void testCrc32() {
@@ -42,6 +49,45 @@ public final class CrcTest {
 		
 		testHex(Crc.CRC32_FUNCTION, "616263"        , "352441C2");
 		testHex(Crc.CRC32_FUNCTION, "616263C2412435", "2144DF1C");
+	}
+	
+	
+	// CRC-32 always detects a 1-bit error, guaranteed.
+	@Test
+	public void testOneBitErrorSmallRandomly() {
+		for (int i = 0; i < 100; i++) {
+			byte[] b = new byte[random.uniformInt(1000) + 1];
+			random.uniformBytes(b);
+			HashValue crc0 = Crc.CRC32_FUNCTION.getHash(b);
+			
+			int j = random.uniformInt(b.length * 8);  // Bit position
+			b[j / 8] ^= 1 << (j % 8);
+			HashValue crc1 = Crc.CRC32_FUNCTION.getHash(b);
+			assertFalse(crc0.equals(crc1));
+		}
+	}
+	
+	
+	// CRC-32 always detects a 1-bit error, guaranteed.
+	@Test
+	public void testOneBitErrorLargeRandomly() {
+		for (int i = 0; i < 10; i++) {
+			int len = random.uniformInt(10000000);
+			int k = random.uniformInt(len * 8);  // Bit position
+			Hasher hasher0 = Crc.CRC32_FUNCTION.newHasher();
+			Hasher hasher1 = Crc.CRC32_FUNCTION.newHasher();
+			byte[] b = new byte[10000];
+			for (int j = 0; j < len; ) {
+				random.uniformBytes(b);
+				int actualLen = Math.min(b.length, len - j);
+				hasher0.update(b, 0, actualLen);
+				if (k / 8 >= j && k / 8 < j + actualLen)
+					b[k / 8 - j] ^= 1 << k % 8;
+				hasher1.update(b, 0, actualLen);
+				j += actualLen;
+			}
+			assertFalse(hasher0.getHash().equals(hasher1.getHash()));
+		}
 	}
 	
 }
