b704c5c9039d15bebc6c44f8471fb57b78dd85ce
Nayuki Minase <nayuki@eigenstate.org>
2013-06-20 05:47:00 +0000

    KeccakHasher: Heavily optimized code by unrolling loops.

--- src/p79068/crypto/hash/KeccakHasher.java
+++ src/p79068/crypto/hash/KeccakHasher.java
@@ -1,5 +1,6 @@
 package p79068.crypto.hash;
 
+import static java.lang.Long.rotateLeft;
 import p79068.Assert;
 import p79068.crypto.Zeroizable;
 import p79068.crypto.Zeroizer;
@@ -20,9 +21,7 @@ class KeccakHasher extends AbstractHasher implements Zeroizable {
 	public KeccakHasher(Keccak hashFunc) {
 		super(hashFunc);
 		state = new long[25];
-		int c = hashFunc.getHashLength() * 8 * 2;
-		int r = 1600 - c;
-		blockSize = r / 8;
+		blockSize = 200 - hashFunc.getHashLength() * 2;
 		blockByteIndex = 0;
 	}
 	
@@ -34,14 +33,51 @@ class KeccakHasher extends AbstractHasher implements Zeroizable {
 		int j = blockByteIndex;
 		if (j < 0 || j >= blockSize)
 			throw new AssertionError();
-		for (int i = off, end = off + len; i < end; i++) {
-			state[j >>> 3] ^= (b[i] & 0xFFL) << ((j & 7) << 3);
-			j++;
+		
+		if ((blockSize & 7) != 0) {
+			// Slow generic code
+			for (int i = off, end = off + len; i < end; i++) {
+				state[j >>> 3] ^= (b[i] & 0xFFL) << ((j & 7) << 3);
+				j++;
+				if (j == blockSize) {
+					absorb();
+					j = 0;
+				}
+			}
+		}
+		else {
+			int i = off;
+			int end = off + len;
+			
+			// Absorb up to 7 input bytes until we hit the next int64 element of 'state'
+			for (; i < end && (j & 7) != 0; i++) {
+				state[j >>> 3] ^= (b[i] & 0xFFL) << ((j & 7) << 3);
+				j++;
+			}
 			if (j == blockSize) {
 				absorb();
 				j = 0;
 			}
+			
+			// Absorb blocks of 8 bytes at a time
+			for (; i + 8 <= end; i += 8) {
+				state[j >>> 3] ^=
+					       (((b[i + 0] & 0xFF) | (b[i + 1] & 0xFF) << 8 | (b[i + 2] & 0xFF) << 16 | b[i + 3] << 24) & 0xFFFFFFFFL)
+					| (long)((b[i + 4] & 0xFF) | (b[i + 5] & 0xFF) << 8 | (b[i + 6] & 0xFF) << 16 | b[i + 7] << 24) << 32;
+				j += 8;
+				if (j == blockSize) {
+					absorb();
+					j = 0;
+				}
+			}
+			
+			// Absorb the remaining bytes, up to 7
+			for (; i < end; i++) {
+				state[j >>> 3] ^= (b[i] & 0xFFL) << ((j & 7) << 3);
+				j++;
+			}
 		}
+		
 		blockByteIndex = j;
 	}
 	
@@ -89,52 +125,92 @@ class KeccakHasher extends AbstractHasher implements Zeroizable {
 		0x8000000080008081L, 0x8000000000008080L, 0x0000000080000001L, 0x8000000080008008L,
 	};
 	
-	private static final int[][] R = {
-		{ 0,  1, 62, 28, 27},
-		{36, 44,  6, 55, 20},
-		{ 3, 10, 43, 25, 39},
-		{41, 45, 15, 21,  8},
-		{18,  2, 61, 56, 14},
-	};
-	
 	
 	private void absorb() {
-		long[] a = state;
-		long[] b = new long[25];
-		long[] c = new long[5];
-		long[] d = new long[5];
+		long a00 = state[ 0], a01 = state[ 1], a02 = state[ 2], a03 = state[ 3], a04 = state[ 4];
+		long a05 = state[ 5], a06 = state[ 6], a07 = state[ 7], a08 = state[ 8], a09 = state[ 9];
+		long a10 = state[10], a11 = state[11], a12 = state[12], a13 = state[13], a14 = state[14];
+		long a15 = state[15], a16 = state[16], a17 = state[17], a18 = state[18], a19 = state[19];
+		long a20 = state[20], a21 = state[21], a22 = state[22], a23 = state[23], a24 = state[24];
+		
 		for (long rc : RC) {
 			// Theta step
-			for (int x = 0; x < 5; x++) {
-				int t = 0;
-				for (int y = 0; y < 5; y++)
-					t ^= a[y * 5 + x];
-				c[x] = t;
-			}
-			
-			for (int x = 0; x < 5; x++)
-				d[x] = c[(x + 4) % 5] ^ Long.rotateLeft(c[(x + 1) % 5], 1);
-			
-			for (int x = 0; x < 5; x++) {
-				for (int y = 0; y < 5; y++)
-					a[y * 5 + x] ^= d[x];
-			}
+			long c0 = a00 ^ a05 ^ a10 ^ a15 ^ a20;
+			long c1 = a01 ^ a06 ^ a11 ^ a16 ^ a21;
+			long c2 = a02 ^ a07 ^ a12 ^ a17 ^ a22;
+			long c3 = a03 ^ a08 ^ a13 ^ a18 ^ a23;
+			long c4 = a04 ^ a09 ^ a14 ^ a19 ^ a24;
+			long d0 = c4 ^ rotateLeft(c1, 1);
+			long d1 = c0 ^ rotateLeft(c2, 1);
+			long d2 = c1 ^ rotateLeft(c3, 1);
+			long d3 = c2 ^ rotateLeft(c4, 1);
+			long d4 = c3 ^ rotateLeft(c0, 1);
+			a00 ^= d0;  a05 ^= d0;  a10 ^= d0;  a15 ^= d0;  a20 ^= d0;
+			a01 ^= d1;  a06 ^= d1;  a11 ^= d1;  a16 ^= d1;  a21 ^= d1;
+			a02 ^= d2;  a07 ^= d2;  a12 ^= d2;  a17 ^= d2;  a22 ^= d2;
+			a03 ^= d3;  a08 ^= d3;  a13 ^= d3;  a18 ^= d3;  a23 ^= d3;
+			a04 ^= d4;  a09 ^= d4;  a14 ^= d4;  a19 ^= d4;  a24 ^= d4;
 			
 			// Rho and pi steps
-			for (int x = 0; x < 5; x++) {
-				for (int y = 0; y < 5; y++)
-					b[(x * 2 + y * 3) % 5 * 5 + y] = Long.rotateLeft(a[y * 5 + x], R[y][x]);
-			}
+			long b00 = rotateLeft(a00,  0);
+			long b16 = rotateLeft(a05, 36);
+			long b07 = rotateLeft(a10,  3);
+			long b23 = rotateLeft(a15, 41);
+			long b14 = rotateLeft(a20, 18);
+			long b10 = rotateLeft(a01,  1);
+			long b01 = rotateLeft(a06, 44);
+			long b17 = rotateLeft(a11, 10);
+			long b08 = rotateLeft(a16, 45);
+			long b24 = rotateLeft(a21,  2);
+			long b20 = rotateLeft(a02, 62);
+			long b11 = rotateLeft(a07,  6);
+			long b02 = rotateLeft(a12, 43);
+			long b18 = rotateLeft(a17, 15);
+			long b09 = rotateLeft(a22, 61);
+			long b05 = rotateLeft(a03, 28);
+			long b21 = rotateLeft(a08, 55);
+			long b12 = rotateLeft(a13, 25);
+			long b03 = rotateLeft(a18, 21);
+			long b19 = rotateLeft(a23, 56);
+			long b15 = rotateLeft(a04, 27);
+			long b06 = rotateLeft(a09, 20);
+			long b22 = rotateLeft(a14, 39);
+			long b13 = rotateLeft(a19,  8);
+			long b04 = rotateLeft(a24, 14);
 			
 			// Chi step
-			for (int x = 0; x < 5; x++) {
-				for (int y = 0; y < 5; y++)
-					a[y * 5 + x] = b[y * 5 + x] ^ (~b[y * 5 + (x + 1) % 5] & b[y * 5 + (x + 2) % 5]);
-			}
-			
-			// Iota step
-			a[0] ^= rc;
+			a00 = b00 ^ (~b01 & b02) ^ rc;  // Iota step
+			a05 = b05 ^ (~b06 & b07);
+			a10 = b10 ^ (~b11 & b12);
+			a15 = b15 ^ (~b16 & b17);
+			a20 = b20 ^ (~b21 & b22);
+			a01 = b01 ^ (~b02 & b03);
+			a06 = b06 ^ (~b07 & b08);
+			a11 = b11 ^ (~b12 & b13);
+			a16 = b16 ^ (~b17 & b18);
+			a21 = b21 ^ (~b22 & b23);
+			a02 = b02 ^ (~b03 & b04);
+			a07 = b07 ^ (~b08 & b09);
+			a12 = b12 ^ (~b13 & b14);
+			a17 = b17 ^ (~b18 & b19);
+			a22 = b22 ^ (~b23 & b24);
+			a03 = b03 ^ (~b04 & b00);
+			a08 = b08 ^ (~b09 & b05);
+			a13 = b13 ^ (~b14 & b10);
+			a18 = b18 ^ (~b19 & b15);
+			a23 = b23 ^ (~b24 & b20);
+			a04 = b04 ^ (~b00 & b01);
+			a09 = b09 ^ (~b05 & b06);
+			a14 = b14 ^ (~b10 & b11);
+			a19 = b19 ^ (~b15 & b16);
+			a24 = b24 ^ (~b20 & b21);
 		}
+		
+		state[ 0] = a00;  state[ 1] = a01;  state[ 2] = a02;  state[ 3] = a03;  state[ 4] = a04;
+		state[ 5] = a05;  state[ 6] = a06;  state[ 7] = a07;  state[ 8] = a08;  state[ 9] = a09;
+		state[10] = a10;  state[11] = a11;  state[12] = a12;  state[13] = a13;  state[14] = a14;
+		state[15] = a15;  state[16] = a16;  state[17] = a17;  state[18] = a18;  state[19] = a19;
+		state[20] = a20;  state[21] = a21;  state[22] = a22;  state[23] = a23;  state[24] = a24;
 	}
 	
 }
