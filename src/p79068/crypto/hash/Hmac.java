fc45cfd3974c8b61c1c6fa8a3b5ed9ef4f5cefd3
Nayuki Minase <nayuki@eigenstate.org>
2013-10-21 01:10:13 +0000

    Hmac: Tweaked and simplified code, updated Javadoc.

--- src/p79068/crypto/hash/Hmac.java
+++ src/p79068/crypto/hash/Hmac.java
@@ -1,5 +1,7 @@
 package p79068.crypto.hash;
 
+import java.util.Arrays;
+
 import p79068.Assert;
 import p79068.crypto.Zeroizable;
 import p79068.hash.AbstractHashFunction;
@@ -8,8 +10,8 @@ import p79068.hash.Hasher;
 
 /**
  * An HMAC (keyed hash message authentication code) hash function.
- * <p>The HMAC specification calls for a block hash function because the initial state of an HMAC instance need not retain incomplete message bytes.</p>
- * <p>The underlying function's block length must not be smaller than its output hash value length.</p>
+ * <p>The HMAC specification requires a block hash function because the initial state of an HMAC instance need not retain incomplete message bytes.</p>
+ * <p>The underlying hash function's block length must not be smaller than its output hash value length.</p>
  * <p>Mutability: <em>Mutable</em></p>
  */
 public final class Hmac extends AbstractHashFunction implements Zeroizable {
@@ -23,29 +25,36 @@ public final class Hmac extends AbstractHashFunction implements Zeroizable {
 	 * Constructs an HMAC algorithm with the specified underlying block hash function and secret key.
 	 * @param hashFunc the underlying block hash function
 	 * @param key the secret key
+	 * @throws IllegalArgumentException if the hash function's block length is smaller than its hash value length
 	 */
 	public Hmac(BlockHashFunction hashFunc, byte[] key) {
 		super(String.format("HMAC-%s", hashFunc.getName()), hashFunc.getHashLength());
-		Assert.assertNotNull(hashFunc, key);
-		if (hashFunc.getBlockLength() < hashFunc.getHashLength())
-			throw new IllegalArgumentException();
+		int blockLen = hashFunc.getBlockLength();
+		if (blockLen < hashFunc.getHashLength())
+			throw new IllegalArgumentException("Block length smaller than hash value length");
+		
+		// Preprocess the key
+		Assert.assertNotNull(key);
+		if (key.length > blockLen)
+			key = hashFunc.getHash(key).toBytes();
+		key = Arrays.copyOf(key, blockLen);
+		
+		// Set initial state of hashers
 		inner = hashFunc.newHasher();
-		outer = hashFunc.newHasher();
-		key = key.clone();
-		key = preprocessKey(hashFunc, key);
 		for (int i = 0; i < key.length; i++)
 			key[i] ^= 0x36;
 		inner.update(key);
+		outer = hashFunc.newHasher();
 		for (int i = 0; i < key.length; i++)
-			key[i] ^= 0x6A;  // Un-XOR by 0x36, then XOR by 0x5C
+			key[i] ^= 0x36 ^ 0x5C;
 		outer.update(key);
 	}
 	
 	
 	
 	/**
-	 * Insert description here.
-	 * @throws IllegalStateException if this object has been zeroized
+	 * Returns a new hasher of this hash function.
+	 * @return a new hasher of this hash function
 	 */
 	@Override
 	public Hasher newHasher() {
@@ -56,7 +65,7 @@ public final class Hmac extends AbstractHashFunction implements Zeroizable {
 	
 	
 	/**
-	 * Insert description here.
+	 * Clears the state of this object for privacy.
 	 * @throws IllegalStateException if this object has been zeroized
 	 */
 	public void zeroize() {
@@ -68,16 +77,6 @@ public final class Hmac extends AbstractHashFunction implements Zeroizable {
 			((Zeroizable)outer).zeroize();
 		inner = null;
 		outer = null;
-	}
-	
-	
-	
-	private static byte[] preprocessKey(BlockHashFunction hashFunc, byte[] key) {
-		if (key.length > hashFunc.getBlockLength())
-			key = hashFunc.getHash(key).toBytes();
-		byte[] blocksizedkey = new byte[hashFunc.getBlockLength()];
-		System.arraycopy(key, 0, blocksizedkey, 0, key.length);
-		return blocksizedkey;
 	}
 	
 }
