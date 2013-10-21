96519a0654eec3c943ec27153bc0818295be670d
Nayuki Minase <nayuki@eigenstate.org>
2013-10-21 00:30:54 +0000

    Zeroizer: Added full Javadoc comments.

--- src/p79068/crypto/Zeroizer.java
+++ src/p79068/crypto/Zeroizer.java
@@ -3,6 +3,12 @@ package p79068.crypto;
 import java.util.Arrays;
 
 
+/**
+ * A utility class for clearing arrays. It is intended to be used like this:
+ * <pre>byte[] b = (...);
+ *(... do work using b ...)
+ *b = Zeroizer.clear(b);  // Sets b to null</pre>
+ */
 public final class Zeroizer {
 	
 	/**
@@ -49,6 +55,11 @@ public final class Zeroizer {
 	}
 	
 	
+	/**
+	 * Sets all subelements in the specified array to zero, sets the elements to {@code null}, and returns {@code null}.
+	 * @param array the array whose contents to clear
+	 * @return {@code null}
+	 */
 	public static byte[][] clear(byte[][] array) {
 		for (int i = 0; i < array.length; i++)
 			array[i] = clear(array[i]);
@@ -56,6 +67,11 @@ public final class Zeroizer {
 	}
 	
 	
+	/**
+	 * Sets all subelements in the specified array to zero, sets the elements to {@code null}, and returns {@code null}.
+	 * @param array the array whose contents to clear
+	 * @return {@code null}
+	 */
 	public static short[][] clear(short[][] array) {
 		for (int i = 0; i < array.length; i++)
 			array[i] = clear(array[i]);
@@ -63,6 +79,11 @@ public final class Zeroizer {
 	}
 	
 	
+	/**
+	 * Sets all subelements in the specified array to zero, sets the elements to {@code null}, and returns {@code null}.
+	 * @param array the array whose contents to clear
+	 * @return {@code null}
+	 */
 	public static int[][] clear(int[][] array) {
 		for (int i = 0; i < array.length; i++)
 			array[i] = clear(array[i]);
@@ -70,6 +91,11 @@ public final class Zeroizer {
 	}
 	
 	
+	/**
+	 * Sets all subelements in the specified array to zero, sets the elements to {@code null}, and returns {@code null}.
+	 * @param array the array whose contents to clear
+	 * @return {@code null}
+	 */
 	public static long[][] clear(long[][] array) {
 		for (int i = 0; i < array.length; i++)
 			array[i] = clear(array[i]);
