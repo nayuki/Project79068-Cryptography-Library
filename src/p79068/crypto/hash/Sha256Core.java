78f5337f9c3c0ed67565bc03417ad7606927c34d
Nayuki Minase <nayuki@eigenstate.org>
2014-08-29 00:59:14 +0000

    FastSha256Core: De-optimized computation method for better compatibility with default JVM options, tweaked superclass field visibility.

--- src/p79068/crypto/hash/Sha256Core.java
+++ src/p79068/crypto/hash/Sha256Core.java
@@ -219,7 +219,7 @@ class Sha256Core extends BlockHasherCore {
 	private static int majority(int x, int y, int z) { return (x & y) ^ (x & z) ^ (y & z); }  // Can be optimized to (x & (y | z)) | (y & z)
 	
 	
-	private static final int[] K = {
+	protected static final int[] K = {
 		0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
 		0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
 		0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
