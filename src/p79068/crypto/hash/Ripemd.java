908847e718d6dd397c0a51d5261d7d97436ec296
Nayuki Minase <nayuki@eigenstate.org>
2014-09-29 04:42:46 +0000

    Ripemd, RipemdCore, RipemdTest: Added classes implementing the RIPEMD-160 hash function.

---
+++ src/p79068/crypto/hash/Ripemd.java
@@ -0,0 +1,33 @@
+package p79068.crypto.hash;
+
+import p79068.hash.Hasher;
+
+
+/**
+ * The RIPEMD-<var>n</var> hash function family.
+ */
+public final class Ripemd extends AbstractBlockHashFunction {
+	
+	/**
+	 * The RIPEMD-160 hash function. {@code name = "RIPEMD-160"}, {@code hashLength = 20}, {@code blockLength = 64}.
+	 */
+	public final static Ripemd RIPEMD160_FUNCTION = new Ripemd("RIPEMD-160", 20);
+	
+	
+	
+	private Ripemd(String name, int hashLen) {
+		super(name, hashLen, 64);
+	}
+	
+	
+	
+	/**
+	 * Returns a new hasher of this hash function.
+	 * @return a new hasher of this hash function
+	 */
+	@Override
+	public Hasher newHasher() {
+		return new BlockHasher(this, new RipemdCore());
+	}
+	
+}
