cc64248cb59f723f50d54f4327c33dd76afde649
Nayuki Minase <nayuki@eigenstate.org>
2015-03-07 15:58:10 +0000

    TeaCipherer, XteaCipherer: Simplified code by merging encrypt() and decrypt().

--- src/p79068/crypto/cipher/TeaCipherer.java
+++ src/p79068/crypto/cipher/TeaCipherer.java
@@ -22,45 +22,17 @@ final class TeaCipherer extends AbstractCipherer {
 	
 	@Override
 	public void encrypt(byte[] b, int off, int len) {
-		if (cipher == null)
-			throw new IllegalStateException("Already zeroized");
-		Assert.assertRangeInBounds(b.length, off, len);
-		if (len % 8 != 0)
-			throw new IllegalArgumentException("Invalid block length");
-		
-		int k0 = int32Key[0];
-		int k1 = int32Key[1];
-		int k2 = int32Key[2];
-		int k3 = int32Key[3];
-		
-		// For each block of 8 bytes
-		for (int end = off + len; off < end; off += 8) {
-			
-			// Pack bytes into int32s in big endian
-			int y = b[off    ] << 24 | (b[off + 1] & 0xFF) << 16 | (b[off + 2] & 0xFF) << 8 | (b[off + 3] & 0xFF);
-			int z = b[off + 4] << 24 | (b[off + 5] & 0xFF) << 16 | (b[off + 6] & 0xFF) << 8 | (b[off + 7] & 0xFF);
-			
-			// The 32 rounds
-			for (int sum = 0x9E3779B9; sum != 0x6526B0D9; sum += 0x9E3779B9) {
-				y += ((z << 4) + k0) ^ (z + sum) ^ ((z >>> 5) + k1);
-				z += ((y << 4) + k2) ^ (y + sum) ^ ((y >>> 5) + k3);
-			}
-			
-			// Unpack int32s into bytes in big endian
-			b[off    ] = (byte)(y >>> 24);
-			b[off + 1] = (byte)(y >>> 16);
-			b[off + 2] = (byte)(y >>>  8);
-			b[off + 3] = (byte)y;
-			b[off + 4] = (byte)(z >>> 24);
-			b[off + 5] = (byte)(z >>> 16);
-			b[off + 6] = (byte)(z >>>  8);
-			b[off + 7] = (byte)z;
-		}
+		crypt(b, off, len, true);
 	}
 	
 	
 	@Override
 	public void decrypt(byte[] b, int off, int len) {
+		crypt(b, off, len, false);
+	}
+	
+	
+	private void crypt(byte[] b, int off, int len, boolean encrypt) {
 		if (cipher == null)
 			throw new IllegalStateException("Already zeroized");
 		Assert.assertRangeInBounds(b.length, off, len);
@@ -80,9 +52,16 @@ final class TeaCipherer extends AbstractCipherer {
 			int z = b[off + 4] << 24 | (b[off + 5] & 0xFF) << 16 | (b[off + 6] & 0xFF) << 8 | (b[off + 7] & 0xFF);
 			
 			// The 32 rounds
-			for (int sum = 0xC6EF3720; sum != 0; sum -= 0x9E3779B9) {
-				z -= ((y << 4) + k2) ^ (y + sum) ^ ((y >>> 5) + k3);
-				y -= ((z << 4) + k0) ^ (z + sum) ^ ((z >>> 5) + k1);
+			if (encrypt) {
+				for (int sum = 0x9E3779B9; sum != 0x6526B0D9; sum += 0x9E3779B9) {
+					y += ((z << 4) + k0) ^ (z + sum) ^ ((z >>> 5) + k1);
+					z += ((y << 4) + k2) ^ (y + sum) ^ ((y >>> 5) + k3);
+				}
+			} else {
+				for (int sum = 0xC6EF3720; sum != 0; sum -= 0x9E3779B9) {
+					z -= ((y << 4) + k2) ^ (y + sum) ^ ((y >>> 5) + k3);
+					y -= ((z << 4) + k0) ^ (z + sum) ^ ((z >>> 5) + k1);
+				}
 			}
 			
 			// Unpack int32s into bytes in big endian
--- src/p79068/crypto/cipher/XteaCipherer.java
+++ src/p79068/crypto/cipher/XteaCipherer.java
@@ -22,41 +22,17 @@ final class XteaCipherer extends AbstractCipherer {
 	
 	@Override
 	public void encrypt(byte[] b, int off, int len) {
-		if (cipher == null)
-			throw new IllegalStateException("Already zeroized");
-		Assert.assertRangeInBounds(b.length, off, len);
-		if (len % 8 != 0)
-			throw new IllegalArgumentException("Invalid block length");
-		
-		// For each block of 8 bytes
-		for (int end = off + len; off < end; off += 8) {
-			
-			// Pack bytes into int32s in big endian
-			int y = b[off    ] << 24 | (b[off + 1] & 0xFF) << 16 | (b[off + 2] & 0xFF) << 8 | (b[off + 3] & 0xFF);
-			int z = b[off + 4] << 24 | (b[off + 5] & 0xFF) << 16 | (b[off + 6] & 0xFF) << 8 | (b[off + 7] & 0xFF);
-			
-			// The 32 rounds
-			for (int sum = 0; sum != 0xC6EF3720; ) {
-				y += (((z << 4) ^ (z >>> 5)) + z) ^ (sum + int32Key[sum & 3]);
-				sum += 0x9E3779B9;
-				z += (((y << 4) ^ (y >>> 5)) + y) ^ (sum + int32Key[(sum >>> 11) & 3]);
-			}
-			
-			// Unpack int32s into bytes in big endian
-			b[off    ] = (byte)(y >>> 24);
-			b[off + 1] = (byte)(y >>> 16);
-			b[off + 2] = (byte)(y >>>  8);
-			b[off + 3] = (byte)y;
-			b[off + 4] = (byte)(z >>> 24);
-			b[off + 5] = (byte)(z >>> 16);
-			b[off + 6] = (byte)(z >>>  8);
-			b[off + 7] = (byte)z;
-		}
+		crypt(b, off, len, true);
 	}
 	
 	
 	@Override
 	public void decrypt(byte[] b, int off, int len) {
+		crypt(b, off, len, false);
+	}
+	
+	
+	private void crypt(byte[] b, int off, int len, boolean encrypt) {
 		if (cipher == null)
 			throw new IllegalStateException("Already zeroized");
 		Assert.assertRangeInBounds(b.length, off, len);
@@ -71,10 +47,18 @@ final class XteaCipherer extends AbstractCipherer {
 			int z = b[off + 4] << 24 | (b[off + 5] & 0xFF) << 16 | (b[off + 6] & 0xFF) << 8 | (b[off + 7] & 0xFF);
 			
 			// The 32 rounds
-			for (int sum = 0xC6EF3720; sum != 0; ) {
-				z -= (((y << 4) ^ (y >>> 5)) + y) ^ (sum + int32Key[(sum >>> 11) & 3]);
-				sum -= 0x9E3779B9;
-				y -= (((z << 4) ^ (z >>> 5)) + z) ^ (sum + int32Key[sum & 3]);
+			if (encrypt) {
+				for (int sum = 0; sum != 0xC6EF3720; ) {
+					y += (((z << 4) ^ (z >>> 5)) + z) ^ (sum + int32Key[sum & 3]);
+					sum += 0x9E3779B9;
+					z += (((y << 4) ^ (y >>> 5)) + y) ^ (sum + int32Key[(sum >>> 11) & 3]);
+				}
+			} else {
+				for (int sum = 0xC6EF3720; sum != 0; ) {
+					z -= (((y << 4) ^ (y >>> 5)) + y) ^ (sum + int32Key[(sum >>> 11) & 3]);
+					sum -= 0x9E3779B9;
+					y -= (((z << 4) ^ (z >>> 5)) + z) ^ (sum + int32Key[sum & 3]);
+				}
 			}
 			
 			// Unpack int32s into bytes in big endian
