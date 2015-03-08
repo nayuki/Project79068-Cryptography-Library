793806672fd2762c7a3f66586c51766e1ee81c39
Nayuki Minase <nayuki@eigenstate.org>
2015-03-08 15:24:00 +0000

    Cipher, BlockCipher: Added and edited Javadoc comments.

--- src/p79068/crypto/cipher/BlockCipher.java
+++ src/p79068/crypto/cipher/BlockCipher.java
@@ -1,24 +1,38 @@
-/* 
- * Block Cipher
- * 
- * Each block must be encrypted independently of other blocks, and the internal state of the cipher must not change.
- * Therefore, a stream cipher is not a block cipher.
- */
-
-
 package p79068.crypto.cipher;
 
 
 /**
  * An abstract block cipher in ECB (electronic codebook) mode.
+ * Each block must be encrypted independently of other blocks,
+ * and the internal state of the cipher must not change.
+ * (Therefore a stream cipher is not a block cipher.)
  * <p>Mutability: <em>Immutable</em>, except for being Zeroizable</p>
  */
 public interface BlockCipher extends Cipher {
 	
 	/**
-	 * Returns a new cipherer, which is used to encrypt and decrypt byte blocks. The cipherer returned must be immutable except for being zeroizable. In particular, its encryption and decryption functions must be pure. That means for encryption, the result must be the same each time the same message is given. The same applies for decryption.
+	 * Returns a new cipherer, which is used to encrypt and decrypt byte blocks.
+	 * The cipherer returned must be immutable except for being zeroizable.
+	 * In particular, its encryption and decryption must be pure functions.
+	 * Thus encrypting (or decrypting) the same block of byte values
+	 * must produce the same result each time.
+	 * @param key the key to be used
+	 * @return a new cipherer of this algorithm with the specified key
 	 */
 	@Override
 	public Cipherer newCipherer(byte[] key);
+	
+	
+	/**
+	 * Returns the block length of this cipher algorithm, in bytes.
+	 * Encryption and decryption only act on complete blocks.
+	 * For a block cipher, this value is typically greater than 1;
+	 * common values are 8 and 16 (respectively 64 and 128 bits).
+	 * <p>This value must not change for the lifetime of
+	 * the cipher object, and must be a positive number.</p>
+	 * @return the block length of this cipher algorithm, in bytes
+	 */
+	@Override
+	public int getBlockLength();
 	
 }
--- src/p79068/crypto/cipher/Cipher.java
+++ src/p79068/crypto/cipher/Cipher.java
@@ -2,13 +2,21 @@ package p79068.crypto.cipher;
 
 
 /**
- * A cipher algorithm. The cipher algorithm should be stateless, except possible for zeroization. The main use of this interface is the method {@link Cipher#newCipherer(byte[])}.
+ * A cipher algorithm. The cipher algorithm should be stateless, except possible for zeroization.
+ * A cipher is given a key to create a {@link Cipherer}, which is used to encrypt/decrypt messages.
+ * <p>Example usage:</p>
+ * <pre>byte[] key = (...);
+ *byte[] msg = (...);
+ *Cipher cp = Rijndael.AES128_CIPHER;
+ *Cipherer c = cp.newCipherer(key);
+ *c.encrypt(msg);</pre>
  * @see Cipherer
  */
 public interface Cipher {
 	
 	/**
-	 * Returns a new cipherer of this algorithm with the specified key, which is used to encrypt and decrypt byte blocks.
+	 * Returns a new cipherer of this algorithm with the specified key,
+	 * which is used to encrypt and decrypt byte blocks.
 	 * @param key the key to be used
 	 * @return a new cipherer of this algorithm with the specified key
 	 */
@@ -16,28 +24,35 @@ public interface Cipher {
 	
 	
 	/**
-	 * Returns the name of this cipher algorithm. The value should not change for the lifetime of the cipher object.
+	 * Returns the name of this cipher algorithm.
+	 * The value must not change for the lifetime of the cipher object.
 	 * @return the name of this cipher algorithm
 	 */
 	public String getName();
 	
 	
 	/**
-	 * Returns the key length of this cipher algorithm, in bytes. The value should not change for the lifetime of the cipher object.
+	 * Returns the key length of this cipher algorithm, in bytes.
+	 * <p>The value must not change for the lifetime of the cipher object, and must be a positive number.</p>
 	 * @return the key length of this cipher algorithm, in bytes
 	 */
 	public int getKeyLength();
 	
 	
 	/**
-	 * Returns the block length of this cipher algorithm, in bytes. Encryptions and decryptions only act on complete blocks. The value should not change for the lifetime of the cipher object.
+	 * Returns the block length of this cipher algorithm, in bytes.
+	 * Encryptions and decryptions only act on complete blocks.
+	 * <p>The value must not change for the lifetime of the cipher object,
+	 * and must be a positive number.</p>
 	 * @return the block length of this cipher algorithm, in bytes
 	 */
 	public int getBlockLength();
 	
 	
 	/**
-	 * Returns a string representation of this cipher. In general, unless otherwise documented, you should assume that the format of the string is not guaranteed to be fixed.
+	 * Returns a string representation of this cipher. In general, unless otherwise documented,
+	 * you should assume that the format of the string is not guaranteed to be fixed.
+	 * This is unlike {@link Cipher#getName()}, which has definite semantics.
 	 * @return a string representation of this cipher
 	 */
 	@Override
